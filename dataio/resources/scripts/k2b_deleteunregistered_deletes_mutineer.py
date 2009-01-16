362b9748cb8c721339aced17e4d25f53f3971ab5
Troy Straszheim <troy@resophonic.com>
2009-01-16 15:40:23 +0000

    Fixes #170 and #137, with new params for Get and some docs
    svn path:/highlander/trunk/; revision:168871

---
+++ dataio/resources/scripts/k2b_deleteunregistered_deletes_mutineer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from I3Tray import *
+from icecube import dataio
+
+tray = I3Tray()
+
+# by default the reader will log_fatal if it can't deserialize something
+tray.AddModule("I3Reader","reader",
+               Filename = "hasmutineer.i3.gz")
+
+tray.AddModule("FrameCheck", "check1",
+               Ensure_Physics_Has = ["mutineer"])
+
+tray.AddModule("DeleteUnregistered", "du")
+
+tray.AddModule("FrameCheck", "check2",
+               Ensure_Physics_Hasnt = ["mutineer"])
+
+tray.AddModule("Get", "getter")
+
+tray.AddModule("Dump","dump")
+tray.AddModule("CountFrames", "count",
+               Physics = 10,
+               Geometry = 1,
+               Calibration = 1,
+               DetectorStatus = 1)
+
+tray.AddModule("TrashCan", "the can")
+
+tray.Execute()
+tray.Finish()
+
+sys.exit(0)
