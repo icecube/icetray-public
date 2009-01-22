ac699b868227f5baa4ee3a4844b7990ae2263740
Troy Straszheim <troy@resophonic.com>
2009-01-22 14:49:21 +0000

    Tests verifying fix of #176
    svn path:/highlander/trunk/; revision:168882

---
+++ dataio/resources/scripts/a2_verify_daqtimes_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from icecube import icetray, dataclasses, dataio
+from I3Tray import *
+from os.path import expandvars
+
+tray = I3Tray()
+
+tools = expandvars("$I3_PORTS")
+runfile = tools + "/test-data/2006data/Run00089508.i3.gz"
+
+tray.AddModule("I3Reader", "read", Filename = runfile)
+
+def SelectDaq(frame):
+	assert 'DrivingTime' in frame
+	dt = frame.Get("DrivingTime")
+	utcdaq = dt.GetUTCDaqTime()
+	print utcdaq
+	assert SelectDaq.daqtime != utcdaq
+	print "Yup prev time %lu different than new time %lu" % (SelectDaq.daqtime, utcdaq)
+	SelectDaq.daqtime = utcdaq
+	return True
+
+SelectDaq.daqtime = 0
+
+tray.AddModule(SelectDaq,"pickdaq",
+	       Streams = [icetray.I3Frame.Physics])
+
+tray.AddModule("Dump","dump")
+
+tray.AddModule("TrashCan","trash")
+tray.Execute()
+tray.Finish()
