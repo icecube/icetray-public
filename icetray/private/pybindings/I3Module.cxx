6e193b208bf53a6d384ffaf7dd98e17376ba451f
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2009-03-30 21:43:01 +0000

    Fix inheritance graph for I3ConditionalModule pybindings. Now it inherits from I3Module in Python, too.
    Long live the C preprocessor!
    
    svn path:/highlander/trunk/; revision:167911

--- icetray/private/pybindings/I3Module.cxx
+++ icetray/private/pybindings/I3Module.cxx
@@ -6,34 +6,49 @@
 using namespace boost::python;
 
 namespace {
+  #define REGMODMETHODS \
+      .def("Configure", &module_t::Configure) \
+      .def("Physics", &module_t::Physics) \
+      .def("Geometry", &module_t::Geometry) \
+      .def("Calibration", &module_t::Calibration) \
+      .def("DetectorStatus", &module_t::DetectorStatus) \
+      .def("Register", &module_t::Register) \
+      .def("AddParameter", \
+	   (void (module_t::*)(const std::string&, const std::string&, const boost::python::object&)) \
+	   &module_t::AddParameter) \
+      .def("AddParameter", \
+	   (void (module_t::*)(const std::string&, const std::string&)) \
+	   &module_t::AddParameter) \
+      .def("GetParameter", &module_t::GetParameter) \
+      .def("Finish", &module_t::Finish) \
+      .def("AddOutBox", &module_t::AddOutBox) \
+      .def("PushFrame", (void (module_t::*)(I3FramePtr)) &module_t::PushFrame) \
+      .def("PushFrame", (void (module_t::*)(I3FramePtr, const std::string&)) &module_t::PushFrame) \
+      .def("PopFrame", &module_t::PopFrame) \
+      .def("Process", &module_t::Process) \
+      .def("RequestSuspension",&module_t::RequestSuspension) \
+
+
   template <typename Base>
   void 
-  go(const char* name)
+  wrapmod(const char *name)
   {
     typedef PythonModule<Base> module_t;
 
     class_<module_t, boost::shared_ptr<module_t>, boost::noncopyable>(name, init<const I3Context&>())
-      .def("Configure", &module_t::Configure)
-      .def("Physics", &module_t::Physics)
-      .def("Geometry", &module_t::Geometry)
-      .def("Calibration", &module_t::Calibration)
-      .def("DetectorStatus", &module_t::DetectorStatus)
-      .def("Register", &module_t::Register)
-      .def("AddParameter", 
-	   (void (module_t::*)(const std::string&, const std::string&, const boost::python::object&))
-	   &module_t::AddParameter)
-      .def("AddParameter", 
-	   (void (module_t::*)(const std::string&, const std::string&))
-	   &module_t::AddParameter)
-      .def("GetParameter", &module_t::GetParameter)
-      .def("Finish", &module_t::Finish)
-      .def("AddOutBox", &module_t::AddOutBox)
-      .def("PushFrame", (void (module_t::*)(I3FramePtr)) &module_t::PushFrame)
-      .def("PushFrame", (void (module_t::*)(I3FramePtr, const std::string&)) &module_t::PushFrame)
-      .def("PopFrame", &module_t::PopFrame)
-      .def("Process", &module_t::Process)
-      .def("RequestSuspension",&module_t::RequestSuspension)
-      ;
+      REGMODMETHODS;
+
+    implicitly_convertible<boost::shared_ptr<module_t>, boost::shared_ptr<I3Module> >();
+  }
+
+  template <typename Base>
+  void 
+  wrapderivedmod(const char *name)
+  {
+    typedef PythonModule<Base> module_t;
+
+    class_<module_t, bases<PythonModule<I3Module> >,boost::shared_ptr<module_t>, boost::noncopyable>(name, init<const I3Context&>())
+      REGMODMETHODS;
 
     implicitly_convertible<boost::shared_ptr<module_t>, boost::shared_ptr<I3Module> >();
   }
@@ -48,6 +63,6 @@ namespace {
 
 void register_I3Module()
 {
-  go<I3Module>("I3Module");
-  go<I3ConditionalModule>("I3ConditionalModule");
+  wrapmod<I3Module>("I3Module");
+  wrapderivedmod<I3ConditionalModule>("I3ConditionalModule");
 }
