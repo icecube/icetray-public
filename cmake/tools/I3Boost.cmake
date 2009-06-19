2056f608ad4d2549aab63a4ca782f73c591e1782
Erik Blaufuss <blaufuss@icecube.umd.edu>
2009-06-19 17:17:58 +0000

    Now require: boost 1.38.0 root 5.18.00 or 5.20.00
    svn path:/highlander/trunk/; revision:169363

--- cmake/tools/I3Boost.cmake
+++ cmake/tools/I3Boost.cmake
@@ -29,10 +29,6 @@ SET(BOOST_DATE_TIME "" CACHE FILEPATH "Cleared with rebuild_cache." FORCE)
 if (NOT BOOST_VERSION)
   if (IS_DIRECTORY ${I3_PORTS}/include/boost-1.38.0)
     set(BOOST_VERSION "1.38.0" CACHE STRING "Boost (from tools) version")
-  elseif (IS_DIRECTORY ${I3_PORTS}/include/boost-1.36.0)
-    set(BOOST_VERSION "1.36.0" CACHE STRING "Boost (from tools) version")
-  elseif(IS_DIRECTORY ${I3_PORTS}/include/boost-1.33.1)
-    set(BOOST_VERSION "1.33.1" CACHE STRING "Boost (from tools) version")
   else()
     set(BOOST_VERSION "NOTFOUND" CACHE STRING "Boost (from tools) version")
   endif()
@@ -41,36 +37,6 @@ endif()
 if (BOOST_VERSION STREQUAL "NOTFOUND")
 
   message(STATUS "Eh, not looking good for boost.  Include directories not found over in $I3_PORTS")
-
-elseif (BOOST_VERSION STREQUAL "1.33.1")
-
-  if(CMAKE_BUILD_TYPE MATCHES "Rel")
-    set(BOOST_LIB_SUFFIX "")
-  else()
-    set(BOOST_LIB_SUFFIX "-d")
-  endif()
-
-
-  TOOLDEF (boost 
-    include/boost-${BOOST_VERSION}
-    boost/version.hpp
-    lib/boost-${BOOST_VERSION}
-    NONE
-    boost_filesystem${BOOST_LIB_SUFFIX} 
-    boost_serialization${BOOST_LIB_SUFFIX} 
-    boost_program_options${BOOST_LIB_SUFFIX} 
-    boost_regex${BOOST_LIB_SUFFIX} 
-    boost_iostreams${BOOST_LIB_SUFFIX} 
-    )
-
-  include_directories(${CMAKE_SOURCE_DIR}/cmake/tool-patches/boost-${BOOST_VERSION})
-
-  set(BOOST_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/boost/public;${BOOST_INCLUDE_DIR})
-  set(BOOST_PYTHON boost_python)
-  set(BOOST_DATE_TIME boost_date_time)
-  set(BOOST_THREAD boost_threads)
-  set(BOOST_SYSTEM boost_system)
-  set(BOOST_SIGNALS boost_signals)
 
 else()
 
