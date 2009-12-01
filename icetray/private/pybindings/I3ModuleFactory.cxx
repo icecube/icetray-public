d37dad857ef52c7a57f2b485e1efbd83bc1e2e43
Troy Straszheim <troy@resophonic.com>
2009-12-01 20:54:13 +0000

    Make it possible to instantiate a wrapped module outside of icetray by setting dummy configs and such
    svn path:/highlander/trunk/; revision:168082

--- icetray/private/pybindings/I3ModuleFactory.cxx
+++ icetray/private/pybindings/I3ModuleFactory.cxx
@@ -19,37 +19,17 @@
 //   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-#include <string>
-#include <icetray/I3Context.h>
-#include <icetray/I3Frame.h>
-#include <icetray/I3Configuration.h>
+#include <icetray/I3Factory.h>
 
 using namespace boost::python;
 
-bool 
-context_putconfig(I3ContextPtr ctx, 
-		  const std::string& where, I3ConfigurationPtr what)
+I3ModulePtr create_module(const std::string& name, I3Context& context)
 {
-  return ctx->Put(where, what);
+  return I3::Singleton<I3ModuleFactory>::get_const_instance()
+    .Create(name)(context);
 }
 
-typedef std::map<std::string, std::pair<FrameFifoPtr, I3ModulePtr> > connections_t;
-
-bool 
-context_putoutboxes(I3ContextPtr ctx, 
-		    const std::string& where, boost::shared_ptr<connections_t> what)
+void register_I3ModuleFactory()
 {
-  return ctx->Put(where, what);
-}
-
-void register_I3Context()
-{
-  class_<I3Context, I3ContextPtr, boost::noncopyable >("I3Context")
-    .def(init<>())
-    .def("__setitem__", &context_putconfig)
-    .def("__setitem__", &context_putoutboxes)
-    ;
-
-  class_<connections_t, boost::shared_ptr<connections_t> >("Connections")
-    ;
+  def("create_module", &create_module);
 }
