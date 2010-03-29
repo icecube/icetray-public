96e54c100f3833e3a3982c986877f792750218e9
Troy Straszheim <troy@resophonic.com>
2010-03-29 20:37:03 +0000

    canned repr, more robust i3_log
    svn path:/highlander/trunk/; revision:168093

--- icetray/private/icetray/impl.h
+++ icetray/private/icetray/impl.h
@@ -1,12 +1,17 @@
 #ifndef ICETRAY_IMPL_H_INCLUDED
 #define ICETRAY_IMPL_H_INCLUDED
 
+#include <boost/python.hpp>
 #include <icetray/I3Logging.h>
 
 #if defined(I3_FRAMEWORK_LOGGING)
-#define i3_log(format, ...) log_trace("%s: " format, GetName().c_str(), ##__VA_ARGS__)
+#define i3_log(format, ...) log_trace("%s: " format, this->GetName().c_str(), ##__VA_ARGS__)
 #else
 #define i3_log(format, ...) 
 #endif
 
+inline std::string repr(const boost::python::object& obj)
+{
+  return boost::python::extract<std::string>(obj.attr("__repr__")());
+}
 #endif
