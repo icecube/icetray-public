8573d8bcdf1eaeb6d1ee55427e591264314a8cb2
Troy Straszheim <troy@resophonic.com>
2010-08-12 13:38:30 +0000

    While debugging something else, an extra test
    svn path:/highlander/trunk/; revision:167980

---
+++ icetray/resources/scripts/pythonfunctionmodule_on_selected_streams_2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from icecube import icetray, dataclasses, dataio, util
+from I3Tray import *
+import os
+
+tray = I3Tray()
+
+#
+#  Will issue frames on the 4 rotating streams
+#
+streams = [icetray.I3Frame.Stream('X'),
+           icetray.I3Frame.Stream('Y'),
+           icetray.I3Frame.Stream('Z')]
+
+tray.AddModule(util.RoundRobinStreams, "bs",
+               Streams = streams
+               )
+
+#
+#   Filter out frames the putint didn't see
+#
+seen = dict(X=0, Y=0, Z=0)
+
+def fn(frame):
+    global seen
+    print "running on frame", frame
+    seen[frame.GetStop().id] += 1
+    
+tray.AddModule(fn, "fn",
+               Streams = streams)
+
+tray.AddModule("Dump", "dump")
+
+tray.AddModule("TrashCan", "tc")
+
+tray.Execute(9)
+tray.Finish()
+
+print seen
+assert seen['X'] == 3
+assert seen['Y'] == 3
+assert seen['Z'] == 3
+    
+
