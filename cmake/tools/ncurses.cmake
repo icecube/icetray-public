808a5489c3d58227878cac5055a1c91df886bac3
nega <nega0@users.noreply.github.com>
2010-09-03 01:39:18 +0000

    previous suse fix broke old behavior.
    restore old behavior. keep suse fix. keep correct behavior.
    thse loops are necessary because cmake's FindCurses.cmake is broken in
    both 2.6 and 2.8. (see its comments and code)
    
    svn path:/highlander/trunk/; revision:169466

--- cmake/tools/ncurses.cmake
+++ cmake/tools/ncurses.cmake
@@ -29,19 +29,34 @@ if(NOT CURSES_FOUND)
 else(NOT CURSES_FOUND)
 
   set(NCURSES_FOUND TRUE)
+  unset(CURSES_NCURSES_INCLUDE_PATH)
+  unset(CURSES_NCURSES_INCLUDE_PATH CACHE)
 
   if(CURSES_HAVE_NCURSES_NCURSES_H)
-    unset(CURSES_NCURSES_INCLUDE_PATH)
-    unset(CURSES_NCURSES_INCLUDE_PATH CACHE)
+
     get_filename_component(CURSES_NCURSES_INCLUDE_PATH ${CURSES_HAVE_NCURSES_NCURSES_H} PATH CACHE)
-    message(STATUS "+ ncurses.h found at ${CURSES_NCURSES_INCLUD_PATH} (this should end with \"ncurses\")")
+    message(STATUS "+ ncurses.h found at ${CURSES_NCURSES_INCLUDE_PATH} (this should end with \"ncurses\")")
     message(STATUS "+ libncurses found at ${CURSES_NCURSES_LIBRARY}")
-  else(CURSES_HAVE_NCURSES_NCURSES_H)
+    set(NCURSES_INCLUDE_DIR ${CURSES_NCURSES_INCLUDE_PATH} 
+      CACHE STRING "Include dir for tool 'ncurses'")
+
+  elseif(CURSES_HAVE_NCURSES_H)
+
+    get_filename_component(CURSES_NCURSES_INCLUDE_PATH ${CURSES_HAVE_NCURSES_H} PATH CACHE)
     message(STATUS "+ ncurses.h found at ${CURSES_NCURSES_INCLUDE_PATH}")
+    message(STATUS "+ libncurses found at ${CURSES_NCURSES_LIBRARY}")
+    set(NCURSES_INCLUDE_DIR ${CURSES_NCURSES_INCLUDE_PATH} 
+      CACHE STRING "Include dir for tool 'ncurses'")
+
+  else(CURSES_HAVE_NCURSES_NCURSES_H)
+
+    message(STATUS "+ ncurses.h found at ${CURSES_INCLUDE_PATH}")
+    message(STATUS "+ libncurses found at ${CURSES_NCURSES_LIBRARY}")
+    set(NCURSES_INCLUDE_DIR ${CURSES_INCLUDE_PATH} 
+      CACHE STRING "Include dir for tool 'ncurses'")
+
   endif(CURSES_HAVE_NCURSES_NCURSES_H)
       
-  set(NCURSES_INCLUDE_DIR ${CURSES_NCURSES_INCLUDE_PATH} 
-    CACHE STRING "Include dir for tool 'ncurses'")
   ## setting this to CURSES_LIBRARIES includes libform
   #set(NCURSES_LIBRARIES ${CURSES_NCURSES_LIBRARY}
   set(NCURSES_LIBRARIES ${CURSES_LIBRARIES}
