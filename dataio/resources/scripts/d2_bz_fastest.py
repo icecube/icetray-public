e39366e51104d8287ebf52eaeddf0c74ce0e020b
Erik Blaufuss <blaufuss@icecube.umd.edu>
2011-01-07 19:34:04 +0000

    Add all of these
    svn path:/highlander/trunk/; revision:168932

---
+++ dataio/resources/scripts/d2_bz_fastest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+#  use the standard messy monolith reader to get some data going.
+#
+from I3Tray import *
+from icecube import icetray, dataclasses, phys_services, dataio
+from os.path import expandvars
+
+import os
+import sys
+
+tray = I3Tray()
+
+tray.AddService("I3ReaderServiceFactory","readerfactory",
+                Filename =  "pass1_best.i3.bz2")
+
+tray.AddModule("I3Muxer","muxme")
+
+tray.AddModule("Dump","dump")
+
+tray.AddModule("I3Writer","writer",
+               filename =  "pass1_fastest.i3.bz2",
+               CompressionLevel =  1)
+
+tray.AddModule("TrashCan", "the can")
+
+tray.Execute()
+tray.Finish()
