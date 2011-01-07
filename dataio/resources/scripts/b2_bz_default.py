b29563e4c2022bfb8b14ce99f44123b9c1f34740
Erik Blaufuss <blaufuss@icecube.umd.edu>
2011-01-07 19:30:44 +0000

    Add some scripts that work out the bz2 compression stuff.
    svn path:/highlander/trunk/; revision:168931

---
+++ dataio/resources/scripts/b2_bz_default.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+#  use the standard messy monolith reader to get some data going.
+#
+from I3Tray import *
+
+from os.path import expandvars
+
+import os
+import sys
+
+load("libdataclasses")
+load("libdataio")
+
+tray = I3Tray()
+
+tray.AddModule("I3Reader","reader",
+               Filename =  "pass1.i3")
+
+tray.AddModule("Dump","dump")
+
+tray.AddModule("I3Writer","writer",
+               filename =  expandvars("pass1_default.i3.bz2"))
+
+tray.AddModule("TrashCan", "the can");
+
+tray.Execute()
+tray.Finish()
