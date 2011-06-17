f3af8b32ac5c13f2d407ca763319b05226ff6bb2
nega <nega0@users.noreply.github.com>
2011-06-17 19:32:05 +0000

    new libarchive detection
    allows the use of system libarchive, but prefers $I3_PORTS
    
    svn path:/highlander/trunk/; revision:169542

--- cmake/tools/libarchive.cmake
+++ cmake/tools/libarchive.cmake
@@ -1,8 +1,27 @@
+colormsg("")
+colormsg(HICYAN "libarchive")
 
-TOOLDEF (libarchive
-  include
-  archive.h
-  lib
-  NONE
-  archive 
-  )
+find_package(PkgConfig)
+pkg_check_modules(PC_LA QUIET libarchive)
+
+find_library(LIBARCHIVE_LIBRARIES archive
+             HINTS ${I3_PORTS}/lib
+                   ${PC_LA_LIBDIR}
+                   ${PC_LA_LIBRARY_DIRS}
+                   ${TOOL_SYSTEM_PATH})
+
+find_path(LIBARCHIVE_INCLUDE_DIR archive.h
+          HINTS ${I3_PORTS}/include
+                ${PC_LA_INCLUDEDIR}
+                ${PC_LA_INCLUDE_DIRS})
+
+if(NOT ${LIBARCHIVE_INCLUDE_DIR} MATCHES "-NOTFOUND")
+  found_ok("archive.h in ${LIBARCHIVE_INCLUDE_DIR}")
+endif(NOT ${LIBARCHIVE_INCLUDE_DIR} MATCHES "-NOTFOUND")
+if(NOT ${LIBARCHIVE_LIBRARIES} MATCHES "-NOTFOUND")
+  found_ok(${LIBARCHIVE_LIBRARIES})
+endif(NOT ${LIBARCHIVE_LIBRARIES} MATCHES "-NOTFOUND")
+
+if(LIBARCHIVE_INCLUDE_DIR AND LIBARCHIVE_LIBRARIES)
+  set(LIBARCHIVE_FOUND TRUE CACHE BOOL "")
+endif(LIBARCHIVE_INCLUDE_DIR AND LIBARCHIVE_LIBRARIES)
