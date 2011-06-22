8f57ddc5a96b7327d168cbc6792725a3249de16e
Alex Olivas <aolivas@umd.edu>
2011-06-22 23:29:15 +0000

    added simple convenience functions to use in python test scripts to mirror the I3Test macros
    svn path:/highlander/trunk/; revision:168178

---
+++ icetray/python/I3Test.py
@@ -0,0 +1,20 @@
+import sys
+
+def ENSURE(cond, msg):
+	if not cond :
+		print msg
+		print "FAIL"
+		sys.exit(1)
+
+def ENSURE_EQUAL(x,y, msg):
+	if not x == y :
+		print msg
+		print "FAIL"
+		sys.exit(1)
+
+def ENSURE_DISTANCE(x,y, d, msg):
+	if abs(x - y) > d :
+		print msg
+		print "FAIL"
+		sys.exit(1)
+
