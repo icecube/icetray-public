d72f77359ebe21aafece4cf1678c3347df73ce39
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-07-05 21:03:41 +0000

    Use cmake BLAS/LAPACK package finders. Those do a better job than the old scripts, and UW Madison has site_cmake overrides for the GotoBLAS case anyway.
    svn path:/highlander/trunk/; revision:169551

--- cmake/tools/blas.cmake
+++ cmake/tools/blas.cmake
@@ -1,8 +1,2 @@
-find_library(BLAS_LIB NAMES goto2 goto acml mkl blas)
-
-if (NOT BLAS_LIB)
-	set(BLAS_FOUND FALSE CACHE BOOL "Tool BLAS found successfully" FORCE)
-else (NOT BLAS_LIB)
-	set(BLAS_LIBRARIES "${BLAS_LIB}" CACHE PATH "Libraries for tool BLAS" FORCE)
-	set(BLAS_FOUND TRUE CACHE BOOL "Tool BLAS found successfully" FORCE)
-endif (NOT BLAS_LIB)
+set(BLA_STATIC BOOL FALSE)
+find_package(BLAS)
