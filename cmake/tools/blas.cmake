2bf49fb2d2b5a4f1760829b06a1e92bbc5106f06
nega <nega0@users.noreply.github.com>
2011-07-05 22:47:19 +0000

    make fortran optional. hopefully.
    svn path:/highlander/trunk/; revision:169557

--- cmake/tools/blas.cmake
+++ cmake/tools/blas.cmake
@@ -1,5 +1,6 @@
 colormsg("")
 colormsg(HICYAN "blas")
 
+enable_language(Fortran OPTIONAL) 
 set(BLA_STATIC FALSE)
 find_package(BLAS)
--- cmake/tools/lapack.cmake
+++ cmake/tools/lapack.cmake
@@ -1,5 +1,6 @@
 colormsg("")
 colormsg(HICYAN "lapack")
 
+enable_language(Fortran OPTIONAL) 
 set(BLA_STATIC FALSE)
 find_package(LAPACK)
