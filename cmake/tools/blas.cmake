990b93424372b525c336f0e151fe7ca255b73c1b
nega <nega0@users.noreply.github.com>
2011-07-09 03:54:53 +0000

    added some detection hints in the comments
    svn path:/highlander/trunk/; revision:169560

--- cmake/tools/blas.cmake
+++ cmake/tools/blas.cmake
@@ -1,5 +1,11 @@
+## If you're having trouble with detection on a debian/ubuntu
+## multiarch system (Ubuntu 11.04+), uncomment the lines around the
+## find_package() call.
+
 colormsg("")
 colormsg(HICYAN "blas")
 
 set(BLA_STATIC FALSE)
+#set(CMAKE_REQUIRED_FLAGS "-L/usr/lib/i386-linux-gnu -L/usr/lib/x86_64-linux-gnu -lgfortran")
 find_package(BLAS)
+#unset(CMAKE_REQUIRED_FLAGS)
--- cmake/tools/lapack.cmake
+++ cmake/tools/lapack.cmake
@@ -1,5 +1,15 @@
+## If you're having trouble with detection on a debian/ubuntu
+## multiarch system (Ubuntu 11.04+), uncomment the lines around the
+## find_package() call.
+
 colormsg("")
 colormsg(HICYAN "lapack")
 
 set(BLA_STATIC FALSE)
+#set(CMAKE_REQUIRED_FLAGS "-L/usr/lib/i386-linux-gnu -L/usr/lib/x86_64-linux-gnu -lgfortran")
 find_package(LAPACK)
+#unset(CMAKE_REQUIRED_FLAGS)
+
+## uncomment the following for debugging...
+#colormsg("BLAS_LIBRARIES: ${BLAS_LIBRARIES}")
+#colormsg("LAPACK_LIBRARIES: ${LAPACK_LIBRARIES}")
