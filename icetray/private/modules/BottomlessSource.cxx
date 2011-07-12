6306178228118bc88100d8eda22c03b62f7eb225
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-07-12 16:55:11 +0000

    Add a streams parameter so that BottomLess source can make non-Physics frames.
    svn path:/highlander/trunk/; revision:168195

--- icetray/private/modules/BottomlessSource.cxx
+++ icetray/private/modules/BottomlessSource.cxx
@@ -26,15 +26,23 @@ struct BottomlessSource : public I3Module
 {
   BottomlessSource(const I3Context& context)  : I3Module(context)
   {
+    AddParameter("Stream", "Frame type to generate", I3Frame::Physics);
     AddOutBox("OutBox");
+  }
+
+  void Configure()
+  {
+    GetParameter("Stream", stream_);
   }
 
   void Process()
   {
     log_trace("%s: %s", GetName().c_str(), __PRETTY_FUNCTION__);
-    I3FramePtr frame(new I3Frame(I3Frame::Physics));
+    I3FramePtr frame(new I3Frame(stream_));
     PushFrame(frame);
   }
+
+  I3Frame::Stream stream_;
 };
 
 I3_MODULE(BottomlessSource);
