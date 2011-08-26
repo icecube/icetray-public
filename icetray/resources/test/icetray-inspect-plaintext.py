dcca76796a1edd61c628692e3c0bb1d8ccdce643
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-08-26 14:54:23 +0000

    -p is obsolete.
    svn path:/highlander/trunk/; revision:168246

--- icetray/resources/test/icetray-inspect-plaintext.py
+++ icetray/resources/test/icetray-inspect-plaintext.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-retval = os.system('icetray-inspect -p icetray dataclasses phys-services')
+retval = os.system('icetray-inspect icetray dataclasses phys-services')
 
 if retval != 0:
     sys.exit(1)
--- icetray/resources/test/icetray-inspect-xml.py
+++ icetray/resources/test/icetray-inspect-xml.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-retval = os.system('icetray-inspect -x -p icetray dataclasses phys-services')
+retval = os.system('icetray-inspect -x icetray dataclasses phys-services')
 
 if retval != 0:
     sys.exit(1)
