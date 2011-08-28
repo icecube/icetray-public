52d103e567dbe3706e345af81be86ff559b2d71b
nega <nega0@users.noreply.github.com>
2011-08-28 07:11:02 +0000

    we're not ready for this yet
    svn path:/highlander/trunk/; revision:169583

--- cmake/tools/jni.cmake
+++ cmake/tools/jni.cmake
@@ -22,16 +22,102 @@
 colormsg("")
 colormsg(HICYAN "jni")
 
-if(NOT IS_DIRECTORY $ENV{JAVA_HOME})
-  colormsg(YELLOW "*** JAVA_HOME ($ENV{JAVA_HOME}) is either unset or does not point to")
-  colormsg(YELLOW "*** a valid directory. This could be a problem if some project in your")
-  colormsg(YELLOW "*** workspace requires java.")
-else()
-  found_ok("JAVA_HOME=$ENV{JAVA_HOME}")
-endif()
+SET(_jni_PATH_SUFFIXES jre/lib/i386 jre/lib/i386/server jre/lib/amd64 jre/lib/amd64/server jre/lib/ia64 jre/lib/ia64/server lib ../Libraries)
 
-find_package(JNI)
+IF(APPLE)
+  FOUND_OK("Using the JavaVM Framework because we're on Apple")
 
-if(JNI_FOUND)
-  set(JNI_FOUND TRUE CACHE BOOL "Jni found flag" FORCE)
-endif(JNI_FOUND)
+  FIND_PATH(jni_h_include_dir
+    NAMES jni.h
+    PATHS
+    /System/Library/Frameworks/JavaVM.framework/Headers
+    $ENV{JAVA_HOME}/include
+    ${TOOL_SYSTEM_PATH})
+  REPORT_FIND(jni jni.h ${jni_h_include_dir})
+
+  FIND_PATH(jni_md_h_include_dir
+    NAMES jni_md.h
+    PATHS
+    $ENV{JAVA_HOME}/include
+    /System/Library/Frameworks/JavaVM.framework/Headers
+    ${TOOL_SYSTEM_PATH})
+  REPORT_FIND(jni jni_md.h ${jni_md_h_include_dir})
+
+#  FIND_LIBRARY(jni_jvm_lib JavaVM)
+#  REPORT_FIND(jni "JavaVM library" ${jni_jvm_lib})
+
+  SET(JNI_LINK_FLAGS "-framework JavaVM")
+  FOUND_OK("Setting JNI_LINK_FLAGS to ${JNI_LINK_FLAGS}")
+
+ELSE(APPLE)
+
+  FIND_PATH(jni_h_include_dir
+    NAMES jni.h
+    PATHS
+    $ENV{JAVA_HOME}/include
+    $ENV{JAVA_HOME}/include/linux
+    ${TOOL_SYSTEM_PATH}
+    )
+  REPORT_FIND(jni jni.h ${jni_h_include_dir})
+
+  FIND_PATH(jni_md_h_include_dir
+    NAMES jni_md.h
+    PATHS
+    $ENV{JAVA_HOME}/include
+    $ENV{JAVA_HOME}/include/linux
+    ${TOOL_SYSTEM_PATH}
+    )
+  REPORT_FIND(jni jni_md.h ${jni_md_h_include_dir})
+
+  #
+  # libjvm
+  #
+  FIND_LIBRARY(jni_jvm_lib
+    NAMES jvm
+    PATHS $ENV{JAVA_HOME}  
+    PATH_SUFFIXES ${_jni_PATH_SUFFIXES}
+    ${TOOL_SYSTEM_PATH}
+    )
+  REPORT_FIND(jni "jvm library" ${jni_jvm_lib})
+
+  #
+  # libverify
+  #
+  FIND_LIBRARY(jni_verify_lib
+    NAMES verify
+    PATHS $ENV{JAVA_HOME}  
+    PATH_SUFFIXES ${_jni_PATH_SUFFIXES}
+    ${TOOL_SYSTEM_PATH}
+    )
+  REPORT_FIND(jni "verify library" ${jni_verify_lib})
+
+  FIND_LIBRARY(jni_zip_lib
+    NAMES zip
+    PATHS $ENV{JAVA_HOME}    
+    PATH_SUFFIXES ${_jni_PATH_SUFFIXES}
+    ${TOOL_SYSTEM_PATH}
+    )
+  REPORT_FIND(jni "zip library" ${jni_zip_lib})
+
+ENDIF(APPLE)
+
+IF(NOT IS_DIRECTORY $ENV{JAVA_HOME})
+  FOUND_NOT_OK("JAVA_HOME ($ENV{JAVA_HOME}) is either unset or does not point to a valid directory.")
+  FOUND_NOT_OK("This is only an error if some project in your workspace requires java.")
+ELSE(NOT IS_DIRECTORY $ENV{JAVA_HOME})
+  FOUND_OK("JAVA_HOME=$ENV{JAVA_HOME}")
+ENDIF(NOT IS_DIRECTORY $ENV{JAVA_HOME})
+
+SET(JNI_INCLUDE_DIR ${jni_h_include_dir} ${jni_md_h_include_dir}
+  CACHE STRING "Include dirs for jni" FORCE)
+
+IF(NOT APPLE)  #if'd because we use Frameworks on Apple
+SET(JNI_LIBRARIES ${jni_jvm_lib} ${jni_verify_lib} ${jni_zip_lib}
+  CACHE STRING "Libraries for tool jni" FORCE)
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+  SET(JNI_LIBRARIES ${jni_jvm_lib} ${jni_jvmlinkage_lib} ${jni_verify_lib} ${jni_zip_lib}
+    CACHE STRING "Libraries for tool jni" FORCE)
+ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+ENDIF(NOT APPLE)
+
+SET(JNI_FOUND TRUE CACHE BOOL "Jni found flag" FORCE)
