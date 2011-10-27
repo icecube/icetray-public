273beb14480b8ed60372a8577e7237a3554667b1
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-10-27 20:57:16 +0000

    Remove redundant include.
    svn path:/highlander/trunk/; revision:168986

--- dataio/private/shovel/Controller.cxx
+++ dataio/private/shovel/Controller.cxx
@@ -39,7 +39,6 @@
 #include <fstream>
 #include <boost/assign/list_inserter.hpp>
 
-#include <boost/archive/portable_binary_iarchive.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
 
