ed3359ddeeee5397e3f5e3a11b7893cca9944829
nega <nega0@users.noreply.github.com>
2011-11-16 22:04:58 +0000

    Test for and use the GotoBLAS2 in I3_PORTS.
    Now that GotoBLAS2 is in ports, and our port of SuiteSparse requires it, prefer
    that one to others. If it's not found, do a normal search. Hopefully, the
    user has a good reason for not using GotoBLAS2 and has overridden all this in
    I3_SITE_CMAKE anyway.
    
    Additionally, only look for BLAS/LAPACK if we're building the icerec meta
    project, since wavedeform is the only project that uses it (via SuiteSparse).
    
    svn path:/highlander/trunk/; revision:169593

--- cmake/tools/blas.cmake
+++ cmake/tools/blas.cmake
@@ -2,10 +2,26 @@
 ## multiarch system (Ubuntu 11.04+), uncomment the lines around the
 ## find_package() call.
 
-colormsg("")
-colormsg(HICYAN "blas")
+if(META_PROJECT MATCHES "^icerec")
 
-set(BLA_STATIC FALSE)
-#set(CMAKE_REQUIRED_FLAGS "-L/usr/lib/i386-linux-gnu -L/usr/lib/x86_64-linux-gnu -lgfortran")
-find_package(BLAS)
-#unset(CMAKE_REQUIRED_FLAGS)
+tooldef(blas
+  include
+  cblas.h
+  lib
+  NONE
+  goto2
+)
+
+if(BLAS_FOUND)
+  set(BLASINI3PORTS_FOUND TRUE)
+else(BLAS_FOUND)
+  colormsg(YELLOW "*** GotoBLAS2 not found in \$I3_PORTS")
+  colormsg(YELLOW "*** Searching for 3rd party BLAS (I hope you linked it against SuiteSparse)")
+
+  set(BLA_STATIC FALSE)
+  #set(CMAKE_REQUIRED_FLAGS "-L/usr/lib/i386-linux-gnu -L/usr/lib/x86_64-linux-gnu -lgfortran")
+  find_package(BLAS)
+  #unset(CMAKE_REQUIRED_FLAGS)
+endif(BLAS_FOUND)
+
+endif(META_PROJECT MATCHES "^icerec")
--- cmake/tools/lapack.cmake
+++ cmake/tools/lapack.cmake
@@ -2,13 +2,29 @@
 ## multiarch system (Ubuntu 11.04+), uncomment the lines around the
 ## find_package() call.
 
-colormsg("")
-colormsg(HICYAN "lapack")
+if(META_PROJECT MATCHES "^icerec")
 
-set(BLA_STATIC FALSE)
-#set(CMAKE_REQUIRED_FLAGS "-L/usr/lib/i386-linux-gnu -L/usr/lib/x86_64-linux-gnu -lgfortran")
-find_package(LAPACK)
-#unset(CMAKE_REQUIRED_FLAGS)
+tooldef(lapack
+  include
+  cblas.h
+  lib
+  NONE
+  goto2
+)
+
+if(LAPACK_FOUND)
+  set(LAPACKINI3PORTS_FOUND TRUE)
+else(LAPACK_FOUND)
+  colormsg(YELLOW "*** GotoBLAS2 not found in \$I3_PORTS")
+  colormsg(YELLOW "*** Searching for 3rd party LAPACK (I hope you linked it against SuiteSparse)")
+
+  set(BLA_STATIC FALSE)
+  #set(CMAKE_REQUIRED_FLAGS "-L/usr/lib/i386-linux-gnu -L/usr/lib/x86_64-linux-gnu -lgfortran")
+  find_package(LAPACK)
+  #unset(CMAKE_REQUIRED_FLAGS)
+endif(LAPACK_FOUND)
+
+endif(META_PROJECT MATCHES "^icerec")
 
 ## uncomment the following for debugging...
 #colormsg("BLAS_LIBRARIES: ${BLAS_LIBRARIES}")
