48397d80af4b6b05c50071637f4d504aca93e749
nega <nega0@users.noreply.github.com>
2012-04-30 14:58:18 +0000

    we really want -O0 when doing coverage otherwise our line counts get screwed up
    svn path:/highlander/trunk/; revision:169641

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -437,18 +437,18 @@ if(NOT METAPROJECT_CONFIGURED)
     "Flags used by compiler during release builds" FORCE)
 
 ## coverage flags
-  SET( CMAKE_CXX_FLAGS_COVERAGE "-pipe -g -fprofile-arcs -ftest-coverage" CACHE STRING
+  SET( CMAKE_CXX_FLAGS_COVERAGE "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
     "Flags used by the C++ compiler during coverage builds."
     FORCE )
-  SET( CMAKE_C_FLAGS_COVERAGE "-pipe -g -fprofile-arcs -ftest-coverage" CACHE STRING
+  SET( CMAKE_C_FLAGS_COVERAGE "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
     "Flags used by the C compiler during coverage builds."
     FORCE )
   SET( CMAKE_EXE_LINKER_FLAGS_COVERAGE
-    "-pipe -g -fprofile-arcs -ftest-coverage" CACHE STRING
+    "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
     "Flags used for linking binaries during coverage builds."
     FORCE )
   SET( CMAKE_SHARED_LINKER_FLAGS_COVERAGE
-    "-pipe -g -fprofile-arcs -ftest-coverage" CACHE STRING
+    "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
     "Flags used by the shared libraries linker during coverage builds."
     FORCE )
   MARK_AS_ADVANCED(
