9a98485cdeb4bf2e42f3c2d75311ea5175ae47b4
nega <nega0@users.noreply.github.com>
2012-06-19 18:50:53 +0000

    fix some cmake style, and a typo that broke clang warnings
    svn path:/highlander/trunk/; revision:169665

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -94,7 +94,7 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
   set(CXX_WARNING_FLAGS "-w1 -Wno-non-virtual-dtor")
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   set(CMAKE_COMPILER_IS_CLANG TRUE)
-  set(CXX_WARNING_FLAGS "-Wall -Wno-non-virtual-dtor -Wno-mismatched-tags -Wno-char-subscripts -Wno-unused -Wunneeded-internal-declaration -Wno-parantheses-equality")
+  set(CXX_WARNING_FLAGS "-Wall -Wno-non-virtual-dtor -Wno-mismatched-tags -Wno-char-subscripts -Wno-unused -Wunneeded-internal-declaration -Wno-parentheses-equality")
 else()
   set(CXX_WARNING_FLAGS "-Wall -Wno-non-virtual-dtor")
 endif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
@@ -442,27 +442,27 @@ if(NOT METAPROJECT_CONFIGURED)
     "Flags used by compiler during release builds" FORCE)
 
 ## coverage flags
-  SET( CMAKE_CXX_FLAGS_COVERAGE "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
+  set(CMAKE_CXX_FLAGS_COVERAGE "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
     "Flags used by the C++ compiler during coverage builds."
     FORCE )
-  SET( CMAKE_C_FLAGS_COVERAGE "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
+  set(CMAKE_C_FLAGS_COVERAGE "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
     "Flags used by the C compiler during coverage builds."
     FORCE )
-  SET( CMAKE_EXE_LINKER_FLAGS_COVERAGE
+  set(CMAKE_EXE_LINKER_FLAGS_COVERAGE
     "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
     "Flags used for linking binaries during coverage builds."
     FORCE )
-  SET( CMAKE_SHARED_LINKER_FLAGS_COVERAGE
+  set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE
     "-pipe -g -O0 -fprofile-arcs -ftest-coverage" CACHE STRING
     "Flags used by the shared libraries linker during coverage builds."
     FORCE )
-  MARK_AS_ADVANCED(
+  mark_as_advanced(
     CMAKE_CXX_FLAGS_COVERAGE
     CMAKE_C_FLAGS_COVERAGE
     CMAKE_EXE_LINKER_FLAGS_COVERAGE
     CMAKE_SHARED_LINKER_FLAGS_COVERAGE )
   # Update the documentation string of CMAKE_BUILD_TYPE for GUIs
-  SET( CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE}" CACHE STRING
+  set(CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE}" CACHE STRING
     "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel Coverage."
     FORCE )
 
