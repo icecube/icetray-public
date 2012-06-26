965cbb2244a5fbf919c88445a4ec1df336fb37da
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2012-06-26 14:13:45 +0000

    Remove pybindings for bits of the context that no longer exist.
    svn path:/highlander/trunk/; revision:168382

--- icetray/private/pybindings/I3Context.cxx
+++ icetray/private/pybindings/I3Context.cxx
@@ -26,37 +26,9 @@
 
 using namespace boost::python;
 
-bool 
-context_putconfig(I3ContextPtr ctx, 
-		  const std::string& where, I3ConfigurationPtr what)
-{
-  return ctx->Put(where, what);
-}
-
-I3ConfigurationPtr
-context_getconfig(I3ContextPtr ctx)
-{
-	return ctx->Get<I3ConfigurationPtr>();
-}
-
-typedef std::map<std::string, std::pair<FrameFifoPtr, I3ModulePtr> > connections_t;
-
-bool 
-context_putoutboxes(I3ContextPtr ctx, 
-		    const std::string& where, boost::shared_ptr<connections_t> what)
-{
-  return ctx->Put(where, what);
-}
-
 void register_I3Context()
 {
   class_<I3Context, I3ContextPtr, boost::noncopyable >("I3Context")
     .def(init<>())
-    .def("__setitem__", &context_putconfig)
-    .def("__setitem__", &context_putoutboxes)
-    .add_property("configuration", &context_getconfig)
-    ;
-
-  class_<connections_t, boost::shared_ptr<connections_t> >("Connections")
     ;
 }
