adc5be5bf2b83c5f4a997546175ea5aa7031a3f3
nega <nega0@users.noreply.github.com>
2012-08-22 17:37:30 +0000

    add new policies for modern cmakes
    svn path:/highlander/trunk/; revision:169678

--- cmake/toplevel.cmake
+++ cmake/toplevel.cmake
@@ -31,6 +31,8 @@ endif("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}")
 
 if(COMMAND cmake_policy)
   cmake_policy(SET CMP0003 NEW)
+  cmake_policy(SET CMP0012 NEW)
+  cmake_policy(SET CMP0017 NEW)
 endif(COMMAND cmake_policy)
 
 #
