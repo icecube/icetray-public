4fef31f44b31823a9ee74260773d385c7352ceb0
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2012-10-20 15:57:50 +0000

    Nothing depends on photonics anymore -- no check to keep checking for it.
    svn path:/highlander/trunk/; revision:169694

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -44,7 +44,7 @@ endif()
 #
 set(ALL_TOOLS pthread root boost python hippodraw
     rdmc expat blas lapack gsl sprng sla libarchive
-    mysql bdb log4cplus mpi photonics suitesparse
+    mysql bdb log4cplus mpi suitesparse
     ZThread omniORB jni ncurses healpix
     cdk qt4 cfitsio hdf5 minuit2 clhep geant4 zlib
     opencl gmp log4cpp xml2 genie zmq
