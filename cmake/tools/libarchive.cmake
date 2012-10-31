1de6c616d40e411b134901df904711886b7884dc
nega <nega0@users.noreply.github.com>
2012-10-31 19:28:56 +0000

    half fixes #IC431. dependencies need to be resolved
    svn path:/highlander/trunk/; revision:169699

--- cmake/tools/libarchive.cmake
+++ cmake/tools/libarchive.cmake
@@ -28,7 +28,7 @@ endif(NOT ${LIBARCHIVE_LIBRARIES} MATCHES "-NOTFOUND")
 
 if(LIBARCHIVE_INCLUDE_DIR AND LIBARCHIVE_LIBRARIES)
   set(LIBARCHIVE_FOUND TRUE CACHE BOOL "")
-  add_custom_command(TARGET install__libs
+  add_custom_command(TARGET install_tool_libs
     PRE_BUILD
     COMMAND mkdir -p ${CMAKE_INSTALL_PREFIX}/lib/tools
     COMMAND ${CMAKE_SOURCE_DIR}/cmake/install_shlib.py ${LIBARCHIVE_LIBRARIES} ${CMAKE_INSTALL_PREFIX}/lib/tools)
