1082f61b40329e6837dfd098880a79bdee06a252
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2012-11-01 18:53:18 +0000

    Remove XXX question: the answer is "No"
    svn path:/highlander/trunk/; revision:169023

--- dataio/private/dataio/I3MultiWriter.cxx
+++ dataio/private/dataio/I3MultiWriter.cxx
@@ -112,7 +112,7 @@ I3MultiWriter::Process()
 
   uint64_t bytes_written;
 
-  // XXX: Is there a way to evaluate file size without the flush?
+  // Need to flush to evaluate file size
   filterstream_.flush();
   io::counter64* ctr = filterstream_.component<io::counter64>(filterstream_.size() - 2);
   if (!ctr) log_fatal("couldnt get counter from stream");
