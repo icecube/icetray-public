e9c5305ed2286c48c9a91002a022a76321d4d503
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2013-02-05 21:34:18 +0000

    C++11 hash map compatibility from Chris Weaver.
    svn path:/highlander/trunk/; revision:169726

--- cmake/tool-patches/common/I3/hash_map.h
+++ cmake/tool-patches/common/I3/hash_map.h
@@ -3,11 +3,9 @@
 
 #include <string>
 
-#if 1 //GCC_VERSION < 40300
+#if __cplusplus < 201103L
+#define USING_GCC_EXT_HASH_MAP 1
 #include <ext/hash_map>
-#else
-#include <unordered_map>
-#endif
 
 namespace __gnu_cxx
 {
@@ -26,5 +24,37 @@ namespace __gnu_cxx
   };
 }
 
+//No template aliases in C++03, so do this the risky way:
+#define hash_map __gnu_cxx::hash_map
+using __gnu_cxx::hash;
+
+#else
+#include <unordered_map>
+
+struct djb2_hash{
+  size_t operator()( const std::string& x ) const
+  {
+    const char* str = x.c_str();
+    // the magic djb2 hash
+    size_t hash = 5381;
+    int c;
+    while ((c = *str++))
+      hash = ((hash << 5) + hash) + c; // hash * 33 + c
+      return hash;
+  }
+};
+
+template<typename T>
+struct i3hash : public std::hash<T>{};
+template<>
+struct i3hash<std::string> : djb2_hash{};
+
+template<typename K, typename V, typename Hash=i3hash<K>>
+using hash_map = std::unordered_map<K,V,Hash>;
+
+template<typename T>
+using hash = i3hash<T>;
+
+#endif // __cplusplus < 201103L
 
 #endif
