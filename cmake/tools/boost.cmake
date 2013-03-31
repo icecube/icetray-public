d8f356cd04f4231b30412368d2fa9d9a75c03546
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2013-03-31 21:11:24 +0000

    Make boost detection a little more cmake idiomatic and use the distribution FindBoost.cmake for both system and non-system boost. Verified to work appropriately on FreeBSD 10 with cmake 2.9. Required features were in place at least as of cmake 2.6.4.
    Behavior is still the same: if SYSTEM_PACKAGES is set, use system boost,
    otherwise use the good old 1.38 from I3_PORTS.
    
    svn path:/highlander/trunk/; revision:169731

--- cmake/tools/boost.cmake
+++ cmake/tools/boost.cmake
@@ -18,68 +18,28 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>
 #  
 
-if(SYSTEM_PACKAGES)
-	find_package(Boost COMPONENTS python)
-endif(SYSTEM_PACKAGES)
+if(NOT SYSTEM_PACKAGES)
+	set(BOOST_PORTSVERSION "1.38.0" CACHE PATH "The boost version.")
+	set(BOOST_INCLUDEDIR ${I3_PORTS}/include/boost-${BOOST_PORTSVERSION})
+	set(BOOST_LIBRARYDIR ${I3_PORTS}/lib/boost-${BOOST_PORTSVERSION})
+	set(Boost_NO_SYSTEM_PATHS TRUE)
+endif(NOT SYSTEM_PACKAGES)
 
-if(Boost_INCLUDE_DIR)
-	SET(BOOST_VERSION "new" CACHE PATH "The boost version.")
-	SET(BOOST_INCLUDE_DIR ${Boost_INCLUDE_DIR} CACHE PATH "Path to the boost include directories." )
-	SET(BOOST_LIBRARY_PATH ${Boost_LIBRARY_DIRS} CACHE PATH "Path to the boost libraries." )
-	# Boost >= 1.42 has no library suffixes on *nix
-	SET(BOOST_LIB_SUFFIX "")
-else(Boost_INCLUDE_DIR)
-	SET(BOOST_VERSION "1.38.0" CACHE PATH "The boost version.")
-	SET(BOOST_INCLUDE_DIR  ${I3_PORTS}/include/boost-${BOOST_VERSION} CACHE PATH "Path to the boost include directories." )
-	SET(BOOST_LIBRARY_PATH  ${I3_PORTS}/lib/boost-${BOOST_VERSION} CACHE PATH "Path to the boost libraries." )
-	if(CMAKE_BUILD_TYPE MATCHES "Rel")
-		set(BOOST_LIB_SUFFIX "-mt")
+colormsg("")
+colormsg(HICYAN "Boost")
+set(Boost_USE_STATIC_LIBS OFF)
+set(Boost_USE_MULTITHREADED ON)
+set(Boost_USE_STATIC_RUNTIME OFF)
+find_package(Boost COMPONENTS python system signals thread date_time serialization filesystem program_options regex iostreams)
+
+if(Boost_FOUND)
+	set(BOOST_FOUND TRUE CACHE BOOL "Boost found successfully" FORCE)
+	set(BOOST_INCLUDE_DIR ${Boost_INCLUDE_DIR} CACHE PATH "Path to the boost include directories.")
+	set(BOOST_LIBRARIES ${Boost_LIBRARIES} CACHE PATH "Boost libraries")
+	if(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake/tool-patches/boost-${BOOST_PORTSVERSION})
+		include_directories(${CMAKE_SOURCE_DIR}/cmake/tool-patches/boost-${BOOST_PORTSVERSION})
 	else()
-		set(BOOST_LIB_SUFFIX "-mt-d")
+		include_directories(${CMAKE_SOURCE_DIR}/cmake/tool-patches/boost-new)
 	endif()
-endif(Boost_INCLUDE_DIR)
-
-# The following prevent these variables from being cached and force 
-#   them to be rechecked across changes from Release/Debug builds.
-SET(BOOST_PYTHON "" CACHE FILEPATH "Cleared with rebuild_cache." FORCE)
-SET(BOOST_SYSTEM "" CACHE FILEPATH "Cleared with rebuild_cache." FORCE)
-SET(BOOST_SIGNALS "" CACHE FILEPATH "Cleared with rebuild_cache." FORCE)
-SET(BOOST_THREAD "" CACHE FILEPATH "Cleared with rebuild_cache." FORCE)
-SET(BOOST_DATE_TIME "" CACHE FILEPATH "Cleared with rebuild_cache." FORCE)
-
-# need to include the right patches 
-if(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake/tool-patches/boost-${BOOST_VERSION})
-  include_directories(${CMAKE_SOURCE_DIR}/cmake/tool-patches/boost-${BOOST_VERSION})
-else()
-  message(ERROR " No tool patch found. ${CMAKE_SOURE_DIR}/cmake/tool-patches/boost-${BOOST_VERSION}.  This likely won't build.")
-endif() 
-
-if (NOT IS_DIRECTORY  ${BOOST_LIBRARY_PATH})
-    message(ERROR " ${BOOST_LIBRARY_PATH} is not a valid directory.")
-endif() 
-
-if (NOT IS_DIRECTORY ${BOOST_INCLUDE_DIR} )
-    message(ERROR " ${BOOST_INCLUDE_DIR} is not a valid directory.")
-endif() 
-
-set(BOOST_ALL_LIBRARIES 
-  boost_python${BOOST_LIB_SUFFIX} 
-  boost_system${BOOST_LIB_SUFFIX} 
-  boost_signals${BOOST_LIB_SUFFIX}  
-  boost_thread${BOOST_LIB_SUFFIX}  
-  boost_date_time${BOOST_LIB_SUFFIX}  
-  boost_serialization${BOOST_LIB_SUFFIX}  
-  boost_filesystem${BOOST_LIB_SUFFIX}  
-  boost_program_options${BOOST_LIB_SUFFIX}  
-  boost_regex${BOOST_LIB_SUFFIX}  
-  boost_iostreams${BOOST_LIB_SUFFIX})
-
-tooldef (boost
-  ${BOOST_INCLUDE_DIR}
-  boost/version.hpp
-  ${BOOST_LIBRARY_PATH}
-  NONE
-  ${BOOST_ALL_LIBRARIES}
-  )
-
+endif(Boost_FOUND)
 
