af2cf86c5964e049c3e64593f4e5351ff19b2f9b
Jakob van Santen <jvansanten@icecube.wisc.edu>
2013-06-03 19:54:04 +0000

    Detect installations of SuiteSparse with non-nested headers. This will instantly break photospline, wavedeform, and millipede once IceRec moves to a new release, but patches are ready.
    svn path:/highlander/trunk/; revision:169744

--- cmake/tools/suitesparse.cmake
+++ cmake/tools/suitesparse.cmake
@@ -1,8 +1,20 @@
 
 TOOLDEF (suitesparse
   include
-  suitesparse/cholmod.h
+  cholmod.h
   lib
   NONE
-  camd ccolamd spqr cholmod amd colamd
+  camd ccolamd spqr cholmod amd colamd tbb suitesparseconfig
+)
+if (NOT SUITESPARSE_FOUND)
+  TOOLDEF (suitesparse
+      include
+      suitesparse/cholmod.h 
+      lib
+      NONE
+      camd ccolamd spqr cholmod amd colamd
   )
+  if (SUITESPARSE_FOUND)
+    LIST(APPEND SUITESPARSE_INCLUDE_DIR ${SUITESPARSE_INCLUDE_DIR}/suitesparse)
+  endif (SUITESPARSE_FOUND)
+endif (NOT SUITESPARSE_FOUND)
