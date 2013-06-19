9147b349cb1d513b9061d92f1932b5afb608f12a
Claudio Kopper <ckopper@icecube.wisc.edu>
2013-06-19 04:00:59 +0000

    prevent warning from shared_ptr when using clang with libc++ in c++03 mode. Happens in boost <= 1.53 (should be fixed in upcoming version 1.54)
    svn path:/highlander/trunk/; revision:169750

--- cmake/tool-patches/boost-new/boost/shared_ptr.hpp
+++ cmake/tool-patches/boost-new/boost/shared_ptr.hpp
@@ -15,7 +15,19 @@
 //  See http://www.boost.org/libs/smart_ptr/shared_ptr.htm for documentation.
 //
 
+#ifdef __clang__
+// prevent warnings from clang in boost <= 1.53 (should be fixed in 1.54)
+// when using shared_ptr with libc++ in c++03 mode
+// (see https://svn.boost.org/trac/boost/ticket/8055 )
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++11-extensions"
+#endif
+
 #include <boost/smart_ptr/shared_ptr.hpp>
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 #else  // #ifndef __CINT__
 namespace boost {
