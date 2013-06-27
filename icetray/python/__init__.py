19f3c113687388cba12b71a3dda535a21a6fe0a4
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2013-06-27 03:55:24 +0000

    Monkeypatch a seg fault when using Icetray (in particular icetray-inspect) with Python 2.7.4 and newer.
    Description of bug:
    If the global logger was set from python, the global shared pointer in
    libicetray.so would be destroyed during shared library unload in
    __cxa_finalize(). At this point, however, the python interpreter has gone
    away already, and the associated Py_DECREF() call will result in an
    explosion. Instead, register an atexit() that turns logging off (actually
    reverts to the default logger, which will be reinstantiated if more logging
    occurs).
    
    svn path:/highlander/trunk/; revision:168525

--- icetray/python/__init__.py
+++ icetray/python/__init__.py
@@ -6,6 +6,13 @@ from traysegment import traysegment, traysegment_inherit, module_altconfig
 from pypick import pypick
 import i3logging as logging
 
+import atexit
+def fix_logging_crash():
+	# Unload any python loggers at exit to prevent Py_DECREF() after
+	# interpreter destruction
+	I3Logger.global_logger = None
+atexit.register(fix_logging_crash)
+
 try:
 	from traydebug import I3TrayDebugger
 except ImportError:
