0c85daf9e94ee2b5d5bda2f21d9bab51b336416d
Jakob van Santen <jvansanten@icecube.wisc.edu>
2013-08-05 16:20:36 +0000

    Make XML2 detection work without I3_PORTS (from dschultz)
    svn path:/highlander/trunk/; revision:169774

--- cmake/tools/xml2.cmake
+++ cmake/tools/xml2.cmake
@@ -1,7 +1,18 @@
-TOOLDEF (xml2
-  include/libxml2-2.7.8/libxml2
-  libxml/parser.h
-  lib/libxml2-2.7.8
-  NONE
-  xml2)
-  
+if (SYSTEM_PACKAGES)
+  find_path(LIBXML2_ROOT_DIR
+    NAMES include/libxml2/libxml/parser.h
+  )
+  TOOLDEF (xml2
+    ${LIBXML2_ROOT_DIR}/include/libxml2
+    libxml/parser.h
+    "lib/libxml2 lib/x86_64-linux-gnu"
+    NONE
+    xml2)
+else (SYSTEM_PACKAGES)
+  TOOLDEF (xml2
+    include/libxml2-2.7.8/libxml2
+    libxml/parser.h
+    lib/libxml2-2.7.8
+    NONE
+    xml2)
+endif (SYSTEM_PACKAGES)
