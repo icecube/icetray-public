b2e085536483edda4bf88cfcdb72d51ede0bab25
Jakob van Santen <jvansanten@icecube.wisc.edu>
2013-09-07 16:01:02 +0000

    Un-nest GMP header path so's we can find it in I3_PORTS / Apt / FreeBSD ports.
    svn path:/highlander/trunk/; revision:169778

--- cmake/tools/gmp.cmake
+++ cmake/tools/gmp.cmake
@@ -1,18 +1,8 @@
 set(GMP_VERSION "4.3.2")
 tooldef(gmp
-  include/gmp-${GMP_VERSION}
-  gmp/gmp.h
+  include/gmp-${GMP_VERSION}/gmp
+  gmp.h
   lib/gmp-${GMP_VERSION}
   NONE
   gmp
   )
-if(NOT GMP_FOUND)
-  set(GMP_LIBRARIES)
-  tooldef(gmp
-    "include/x86_64-linux-gnu"
-    gmp.h
-    "lib/x86_64-liunx-gnu"
-    NONE
-    gmp
-    )
-endif(NOT GMP_FOUND)
