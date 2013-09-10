f4b020060481c3fa76505485103e8a8c471b94a9
nega <nega0@users.noreply.github.com>
2013-09-10 19:46:45 +0000

    search more aggressively under SYSTEM_PACKAGES
    svn path:/highlander/trunk/; revision:169780

--- cmake/tools/gmp.cmake
+++ cmake/tools/gmp.cmake
@@ -1,8 +1,20 @@
-set(GMP_VERSION "4.3.2")
-tooldef(gmp
-  include/gmp-${GMP_VERSION}/gmp
-  gmp.h
-  lib/gmp-${GMP_VERSION}
-  NONE
-  gmp
+if (SYSTEM_PACKAGES)
+  find_path(GMP_INCLUDE_DIR
+    NAMES include/gmp.h)
+  tooldef(gmp
+    ${GMP_INCLUDE_DIR}
+    gmp.h
+    lib
+    NONE
+    gmp
   )
+else (SYSTEM_PACKAGES)
+  set(GMP_VERSION "4.3.2")
+  tooldef(gmp
+    include/gmp-${GMP_VERSION}/gmp
+    gmp.h
+    lib/gmp-${GMP_VERSION}
+    NONE
+    gmp
+  )
+endif (SYSTEM_PACKAGES)
