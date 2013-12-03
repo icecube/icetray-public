d4487d8bfcd6604af26933768bf640e0a85a8b47
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2013-12-03 20:10:05 +0000

    Fall back on "include" and "lib" if there is no versioned GSL subdirectory.
    svn path:/highlander/trunk/; revision:169800

--- cmake/tools/gsl.cmake
+++ cmake/tools/gsl.cmake
@@ -22,13 +22,14 @@ IF(NOT GSL_VERSION)
   foreach(v 1.8 1.14 1.15)
     if(IS_DIRECTORY ${I3_PORTS}/include/gsl-${v})
       set(GSL_VERSION "${v}")
+      set(GSL_SUBDIR "gsl-${v}")
     endif(IS_DIRECTORY ${I3_PORTS}/include/gsl-${v})
   endforeach(v 1.8 1.14 1.15)
 ENDIF(NOT GSL_VERSION)
 
 if(BLAS_FOUND)
   tooldef(gsl 
-    include/gsl-${GSL_VERSION}
+    include/${GSL_SUBDIR}
     gsl/gsl_rng.h
     lib/gsl-${GSL_VERSION}
     NONE
@@ -36,9 +37,9 @@ if(BLAS_FOUND)
   list(APPEND GSL_LIBRARIES ${BLAS_LIBRARIES})
 else(BLAS_FOUND)
   tooldef(gsl 
-    include/gsl-${GSL_VERSION}
+    include/${GSL_SUBDIR}
     gsl/gsl_rng.h
-    lib/gsl-${GSL_VERSION}
+    lib/${GSL_SUBDIR}
     NONE
     gsl gslcblas)
 endif(BLAS_FOUND)
