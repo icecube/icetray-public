2ed949b201b86cf075ea729071e367399033ed11
Jakob van Santen <jvansanten@icecube.wisc.edu>
2014-04-23 20:09:05 +0000

    Find a standalone cblas library if the blas library doesn't include the C API. Also pass along the right link flags to gsl.
    svn path:/highlander/trunk/; revision:169832

--- cmake/tools/gsl.cmake
+++ cmake/tools/gsl.cmake
@@ -35,6 +35,7 @@ if(BLAS_FOUND)
     NONE
     gsl)
   list(APPEND GSL_LIBRARIES ${BLAS_LIBRARIES})
+  list(APPEND GSL_LINK_FLAGS ${BLAS_LINK_FLAGS})
 else(BLAS_FOUND)
   tooldef(gsl 
     include/${GSL_SUBDIR}
