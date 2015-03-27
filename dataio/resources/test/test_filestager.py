874484e968bc8eb65a7fc28b2ff6ded84259d23e
David Schultz <davids24@gmail.com>
2015-03-27 17:43:50 +0000

    fully local testing of file stager, using a test web server
    svn path:/highlander/trunk/; revision:169134

--- dataio/resources/test/test_filestager.py
+++ dataio/resources/test/test_filestager.py
@@ -2,7 +2,7 @@
 
 import sys
 if sys.version_info[:2] < (2,6):
-	sys.exit(0)
+    sys.exit(0)
 
 from icecube import dataio
 from icecube.dataio import I3FileStagerFile
@@ -15,62 +15,173 @@ icetray.logging.set_level('TRACE')
 dataio.set_local_scratch_dir('.')
 
 def test_scratchdir(url=os.path.expandvars("file://$I3_BUILD/env-shell.sh")):
-	stager = I3FileStagerFile()
-	# directory is created lazily
-	scratch_dir = stager.scratch_dir
-	assert(scratch_dir is None)
-	# now it should exist
-	stager.GetReadablePath(url)
-	scratch_dir = stager.scratch_dir
-	assert(os.path.isdir(scratch_dir))
-	del stager
-	# now it should be gone
-	assert(not os.path.isdir(scratch_dir))
+    stager = I3FileStagerFile()
+    # directory is created lazily
+    scratch_dir = stager.scratch_dir
+    assert(scratch_dir is None)
+    # now it should exist
+    stager.GetReadablePath(url)
+    scratch_dir = stager.scratch_dir
+    assert(os.path.isdir(scratch_dir))
+    del stager
+    # now it should be gone
+    assert(not os.path.isdir(scratch_dir))
 
 def _test_stage(url, minsize=100):
-	stager = I3FileStagerFile()
-	local_fname = stager.GetReadablePath(url)
-	assert(os.path.exists(str(local_fname)))
-	assert(os.stat(str(local_fname)).st_size > minsize)
-	local_fname = str(local_fname)
-	# check that staged files are really deleted
-	if stager.CanStageIn(url):
-		assert(not os.path.exists(str(local_fname)))
+    stager = I3FileStagerFile()
+    local_fname = stager.GetReadablePath(url)
+    assert(os.path.exists(str(local_fname)))
+    assert(os.stat(str(local_fname)).st_size > minsize)
+    local_fname = str(local_fname)
+    # check that staged files are really deleted
+    if stager.CanStageIn(url):
+        assert(not os.path.exists(str(local_fname)))
+
+def _make_http(port=8888,usessl=False,basic_auth=False):
+    import BaseHTTPServer
+    import ssl
+    import threading
+    import subprocess
+    
+    data = ''.join(['test' for _ in range(1000)])
+    
+    if basic_auth:
+        class Handle(BaseHTTPServer.BaseHTTPRequestHandler):
+            def do_HEAD(self):
+                self.send_response(200)
+                self.send_header('Content-type','text')
+                self.end_headers()
+            def do_AUTHHEAD(self):
+                self.send_response(401)
+                self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
+                self.send_header('Content-type','text')
+                self.end_headers()
+            def do_GET(self):
+                if self.headers.getheader('Authorization') == None:
+                    self.do_AUTHHEAD()
+                    self.wfile.write('no auth header received')
+                elif self.headers.getheader('Authorization') == 'Basic dGVzdDp0ZXN0':
+                    self.do_HEAD()
+                    self.wfile.write(self.headers.getheader('Authorization'))
+                    self.wfile.write(data)
+                else:
+                    self.do_AUTHHEAD()
+                    self.wfile.write(self.headers.getheader('Authorization'))
+                    self.wfile.write('not authenticated')
+    else:
+        class Handle(BaseHTTPServer.BaseHTTPRequestHandler):
+            def do_HEAD(self):
+                self.send_response(200)
+                self.send_header('Content-type','text')
+                self.end_headers()
+            def do_GET(self):
+                self.do_HEAD()
+                self.wfile.write(data)
+                
+    httpd = BaseHTTPServer.HTTPServer(('localhost', port), Handle)
+    if usessl:
+        print 'ssl'
+        p = subprocess.Popen(['openssl','req','-new','-x509',
+                              '-keyout','privkey.pem',
+                              '-out','cacert.pem','-days','1',
+                              '-batch','-passout','pass:passkey'],
+                              stdin=subprocess.PIPE)
+        p.communicate(input='passkey')
+        if p.returncode:
+            raise Exception('cannot generate self-signed cert')
+        try:
+            if subprocess.call(['openssl','rsa','-in','privkey.pem','-out','key.pem','-passin','pass:passkey']):
+                raise Exception('error removing password from key.pem')
+            open('key.pem','a').write(open('cacert.pem').read())
+            httpd.socket = ssl.wrap_socket(httpd.socket, certfile='key.pem',
+                                           server_side=True)
+        except Exception:
+            os.remove('privkey.pem')
+            os.remove('cacert.pem')
+            if os.path.exists('key.pem'):
+                os.remove('key.pem')
+            raise
+
+    t = threading.Thread(target=httpd.serve_forever)
+    t.start()
+    def shutdown():
+        httpd.shutdown()
+        t.join()
+        if os.path.exists('privkey.pem'):
+            os.remove('privkey.pem')
+        if os.path.exists('key.pem'):
+            os.remove('key.pem')
+        if os.path.exists('cacert.pem'):
+            os.remove('cacert.pem')
+    return shutdown
+
 
 def test_double_stage():
-	"""
-	Stager should download files only once and return a handle to the existing
-	file if it's still live
-	"""
-	url = "http://code.icecube.wisc.edu/tools/clsim/MD5SUMS"
-	stager = I3FileStagerFile()
-	f1 = stager.GetReadablePath(url)
-	f2 = stager.GetReadablePath(url)
-	assert(str(f1) == str(f2))
-	assert(os.path.exists(str(f1)))
-	assert(os.path.exists(str(f2)))
-	del f1
-	assert(os.path.exists(str(f2)))
-	f2path = str(f2)
-	del f2
-	assert(not os.path.exists(f2path))
+    """
+    Stager should download files only once and return a handle to the existing
+    file if it's still live
+    """
+    port = 12345
+    url = "http://localhost:%d"%port
+    stop = _make_http(port=port)
+    try:
+        stager = I3FileStagerFile()
+        f1 = stager.GetReadablePath(url)
+        f2 = stager.GetReadablePath(url)
+        assert(str(f1) == str(f2))
+        assert(os.path.exists(str(f1)))
+        assert(os.path.exists(str(f2)))
+        del f1
+        assert(os.path.exists(str(f2)))
+        f2path = str(f2)
+        del f2
+        assert(not os.path.exists(f2path))
+    finally:
+        stop()
 
 def test_http():
-	_test_stage("http://code.icecube.wisc.edu/tools/clsim/MD5SUMS")
+    port = 12345
+    address = "http://localhost:%d"%port
+    stop = _make_http(port=port)
+    try:
+        _test_stage(address)
+    finally:
+        stop()
 
 def test_http_with_auth():
-	_test_stage("http://icecube:skua@convey.icecube.wisc.edu/data/sim/sim-new/downloads/globus.tar.gz.md5sum", 32)
+    port = 12345
+    address = "http://test:test@localhost:%d"%port
+    stop = _make_http(port=port,basic_auth=True)
+    try:
+        _test_stage(address)
+    finally:
+        stop()
 
 def test_https():
-	_test_stage("https://icecube.wisc.edu/index.html")
+    port = 12345
+    address = "https://localhost:%d"%port
+    stop = _make_http(port=port,usessl=True)
+    try:
+        _test_stage(address)
+    finally:
+        stop()
+
+def test_https_with_auth():
+    port = 12345
+    address = "https://test:test@localhost:%d"%port
+    stop = _make_http(port=port,usessl=True,basic_auth=True)
+    try:
+        _test_stage(address)
+    finally:
+        stop()
 
 def test_file():
-	_test_stage(os.path.expandvars("file://$I3_BUILD/env-shell.sh"))
-	
+    _test_stage(os.path.expandvars("file://$I3_BUILD/env-shell.sh"))
+    
 def test_file_implicit():
-	_test_stage(os.path.expandvars("$I3_BUILD/env-shell.sh"))
+    _test_stage(os.path.expandvars("$I3_BUILD/env-shell.sh"))
 
 if __name__ == "__main__":
-	for k, v in list(locals().items()):
-		if k.startswith("test") and hasattr(v, "__call__"):
-			v()
+    for k, v in list(locals().items()):
+        if k.startswith("test") and hasattr(v, "__call__"):
+            v()
