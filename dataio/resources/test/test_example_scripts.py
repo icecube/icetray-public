5ceaac308b31bbe973126280174bbe71d159051a
David Schultz <davids24@gmail.com>
2015-08-31 17:06:28 +0000

    remove event hunter, as it relies on dataclasses
    svn path:/highlander/trunk/; revision:169167

--- dataio/resources/test/test_example_scripts.py
+++ dataio/resources/test/test_example_scripts.py
@@ -15,17 +15,7 @@ class TestSimpleExample(I3Test.TestExampleScripts):
 
     def test_scan(self):
         input_file = self.I3_TESTDATA + "/sim/corsika.F2K010001_IC59_slim.i3.gz"
-        output_file = "./output.i3.gz"
-        self.run_example('strip.py', input_file, output_file)
-
-# Some day this will work
-    def test_event_hunter(self):
-        input_file = self.I3_TESTDATA + "/sim/corsika.F2K010001_IC59_slim.i3.gz"
-        output_file = "./output.i3.gz"
-        self.run_example('event_hunter.py',
-                         '--event=0', 
-                         '--output=output.i3.gz',
-                         input_file)
+        self.run_example('scan.py', '-c', input_file)
 
     def tearDown(self):
         '''
@@ -33,5 +23,5 @@ class TestSimpleExample(I3Test.TestExampleScripts):
         '''
         if os.path.exists("./output.i3.gz"):
             os.remove("./output.i3.gz")
-        
+
 unittest.main()
