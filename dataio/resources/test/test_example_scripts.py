c51884b5ab44ec9546cebcbd8df1f2bf7d753667
Alex Olivas <aolivas@umd.edu>
2015-08-31 03:32:57 +0000

    the test that runs event_hunter.py doesn't crash anymore, but i'm still not convinced i'm using it correctly.  need to move on though.
    svn path:/highlander/trunk/; revision:169165

--- dataio/resources/test/test_example_scripts.py
+++ dataio/resources/test/test_example_scripts.py
@@ -19,12 +19,13 @@ class TestSimpleExample(I3Test.TestExampleScripts):
         self.run_example('strip.py', input_file, output_file)
 
 # Some day this will work
-#    def test_event_hunter(self):
-#        input_file = self.I3_TESTDATA + "/sim/corsika.F2K010001_IC59_slim.i3.gz"
-#        output_file = "./output.i3.gz"
-#        self.run_example('event_hunter.py',
-#                         '--event=0', '--input-file=%s' % input_file,
-#                         '--output=output.i3.gz')
+    def test_event_hunter(self):
+        input_file = self.I3_TESTDATA + "/sim/corsika.F2K010001_IC59_slim.i3.gz"
+        output_file = "./output.i3.gz"
+        self.run_example('event_hunter.py',
+                         '--event=0', 
+                         '--output=output.i3.gz',
+                         input_file)
 
     def tearDown(self):
         '''
