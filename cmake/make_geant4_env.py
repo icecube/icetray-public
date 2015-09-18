9ef88a3724107177633d3c314231c97a38f1982e
Hans Dembinski <hans.dembinski@icecube.wisc.edu>
2015-09-18 17:09:19 +0000

    fix for python2.6
    svn path:/highlander/trunk/; revision:170130

--- cmake/make_geant4_env.py
+++ cmake/make_geant4_env.py
@@ -46,7 +46,7 @@ if os.path.isfile(geant4_sh):
 # if this fails, try heuristic to fetch the directories from I3_PORTS
 if not geant4_env:
     i3ports = os.environ["I3_PORTS"]
-    cmd = "find {}/share -type d".format(i3ports)
+    cmd = "find {0}/share -type d".format(i3ports)
     out = subp.Popen(cmd.split(), stdout=subp.PIPE).communicate()[0]
 
     # collect all candidates
@@ -64,7 +64,7 @@ if not geant4_env:
     patch = geant4_version[-1]
     minor = geant4_version[-2]
     major = geant4_version[:-2]
-    g4version = "{}.{}.{}".format(major, minor, patch)
+    g4version = "{0}.{1}.{2}".format(major, minor, patch)
     for var in candidates:
         for c in candidates[var]:
             geant4_env[var] = c
@@ -80,17 +80,17 @@ for var in geant4_vars:
         # but don't complain if we are just running inside an env-shell.sh
         value = os.environ[var]
         if not "I3_SHELL" in os.environ:
-            sys.stderr.write(("Warning: Geant4 environment variable already set {}={}, "
+            sys.stderr.write(("Warning: Geant4 environment variable already set {0}={1}, "
                               "this overrides automatic detection\n")
                               .format(var, value))
     elif var in geant4_env:
         value = geant4_env[var]
 
     if value is None:
-        sys.stderr.write(("Warning: Geant4 environment variable {} could not be set, "
+        sys.stderr.write(("Warning: Geant4 environment variable {0} could not be set, "
                           "g4-based modules may crash\n").format(var))
     else:
-        formatted_pairs.append("{}={}".format(var, value))           
+        formatted_pairs.append("{0}={1}".format(var, value))           
 
 # extra formatting for env-shell.sh        
 sys.stdout.write(" \\\n\t".join(formatted_pairs))
