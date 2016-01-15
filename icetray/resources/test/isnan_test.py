2a31e026141d9d218d44c2a722983967bd008908
nega <nega0@users.noreply.github.com>
2016-01-15 16:24:36 +0000

    remove hand rolled isnan. python 2.6 and numpy are required. use those isnans
    svn path:/highlander/trunk/; revision:168681

--- icetray/resources/test/isnan_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from icecube import i3math
-from icecube.icetray.I3Test import *
-
-ENSURE( i3math.isnan( float('NaN') ), "isnan( float('NaN') ) should evaluate to True." )
-ENSURE( not i3math.isnan( 1.0 ), "isnan( 1.0 ) should evaluate to False." )
-
