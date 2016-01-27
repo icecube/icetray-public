df86c8b5f330c0b942fe3b51bef1b0f7931a5bf1
David Schultz <davids24@gmail.com>
2016-01-27 19:22:50 +0000

    increase coverage of pybindings
    svn path:/highlander/trunk/; revision:168683

--- icetray/resources/test/i3bool_compare.py
+++ icetray/resources/test/i3bool_compare.py
@@ -7,3 +7,13 @@ ENSURE( icetray.I3Bool(False) == False , "I3Bool == False failed")
 
 ENSURE( icetray.I3Bool(True) != False , "I3Bool != False failed")
 ENSURE( icetray.I3Bool(False) != True , "I3Bool != True failed")
+
+b = repr(icetray.I3Bool(True))
+ENSURE( 'true' in b.lower(), "I3Bool does not repr true")
+b = repr(icetray.I3Bool(False))
+ENSURE( 'false' in b.lower(), "I3Bool does not repr false")
+
+b = icetray.I3Bool(True).__str__()
+ENSURE( 'true' in b.lower(), "I3Bool does not print true")
+b = icetray.I3Bool(False).__str__()
+ENSURE( 'false' in b.lower(), "I3Bool does not print false")
