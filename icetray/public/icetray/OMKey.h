ff6cb80644f1f3968b649cbd141b6d4f1b0a3718
Hans Dembinski <hans.dembinski@icecube.wisc.edu>
2016-03-30 17:49:51 +0000

    extending docu on PMT number
    svn path:/highlander/trunk/; revision:168720

--- icetray/public/icetray/OMKey.h
+++ icetray/public/icetray/OMKey.h
@@ -33,9 +33,10 @@ static const unsigned omkey_version_ = 2;
 /**
  * @brief A small class which is the string number, om number
  * and pmt number for a specific PMT inside a DOM.
- * For IceCube, the PMT number will always be "0"
- * and "PMT" is equivalent to "DOM".
  *
+ * For IceCube, the PMT number will always be 0
+ * and "PMT" is equivalent to "DOM". For IceTop, the PMT number
+ * can be 0 or 1.
  */
 class OMKey 
 {
