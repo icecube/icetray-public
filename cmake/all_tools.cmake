fddfb6d75cb898b1c9123ef3b7cf878ebb188ac9
Kevin Meagher <kmeagher@icecube.wisc.edu>
2016-04-21 15:53:36 +0000

    add tool for NLopt minimization library
    svn path:/highlander/trunk/; revision:170243

--- cmake/all_tools.cmake
+++ cmake/all_tools.cmake
@@ -6,7 +6,7 @@ set(ALL_TOOLS pthread root boost boostnumpy python
     mysql bdb mpi suitesparse ZThread omniORB ncurses cdk
     healpix qt4 cfitsio hdf5 minuit2 clhep geant4 zlib
     opencl gmp log4cpp xml2 genie zmq doxygen
-    multinest
+    multinest nlopt
     ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
     )
 list(REMOVE_DUPLICATES ALL_TOOLS)
