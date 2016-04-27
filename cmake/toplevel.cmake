79967bc65ffbaa64bbca157d044e1102700f542e
nega <nega0@users.noreply.github.com>
2016-04-27 17:43:35 +0000

    minor comments and whitespace cleanup
    svn path:/highlander/trunk/; revision:170257

--- cmake/toplevel.cmake
+++ cmake/toplevel.cmake
@@ -1,21 +1,21 @@
 #  $Id$
-#  
+#
 #  Copyright (C) 2007   Troy D. Straszheim  <troy@icecube.umd.edu>
 #  and the IceCube Collaboration <http://www.icecube.wisc.edu>
-#  
+#
 #  This file is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>
-#  
+#
 cmake_minimum_required(VERSION 2.8.5 FATAL_ERROR)
 if("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}")
   message(FATAL_ERROR "
@@ -58,9 +58,9 @@ set(I3_BUILD ${CMAKE_BINARY_DIR})
 include(${CMAKE_SOURCE_DIR}/CMakeLists.optional.txt OPTIONAL)
 
 set(CMAKE_MODULE_PATH
-  ${EXTRA_CMAKE_MODULE_PATH} 
-  ${CMAKE_SOURCE_DIR}/cmake 
-  ${CMAKE_SOURCE_DIR}/cmake/tools 
+  ${EXTRA_CMAKE_MODULE_PATH}
+  ${CMAKE_SOURCE_DIR}/cmake
+  ${CMAKE_SOURCE_DIR}/cmake/tools
   ${CMAKE_SOURCE_DIR}/cmake/utility)
 
 include(utility)
@@ -136,7 +136,7 @@ install(PROGRAMS ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/tarball-env-shell.s
   RENAME env-shell.sh
   )
 
-add_custom_target(env-check ALL 
+add_custom_target(env-check ALL
   COMMAND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/env-check.sh
   COMMENT "Checking build against environment")
 
@@ -243,29 +243,32 @@ configure_file(
   )
 execute_process(COMMAND chmod 755 ${CMAKE_BINARY_DIR}/bin/icetray-config)
 execute_process(COMMAND cp ${CMAKE_BINARY_DIR}/bin/icetray-config ${NOTES_DIR})
-
 exec_program(${CMAKE_BINARY_DIR}/env-shell.sh ARGS /usr/bin/env > ${NOTES_DIR}/env-post_shell.txt OUTPUT_VARIABLE DEV_NULL)
 
+#
+#  gfilt configuration
+#
 message(STATUS "Configuring 'gfilt' STL decryptor")
 configure_file(
   ${CMAKE_SOURCE_DIR}/cmake/gfilt.in
   ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/gfilt
   @ONLY
   )
-set(GFILT_PROGRAM 
+set(GFILT_PROGRAM
   ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/gfilt
   CACHE FILEPATH "Gfilt error filter path")
-
-  
 find_program(GFILT_PROGRAM gfilt PATHS ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY} NO_DEFAULT_PATH)
 
+#
+#  dpkg configuration
+#
 if(DPKG_INSTALL_PREFIX)
   install(DIRECTORY ${DOXYGEN_OUTPUT_PATH} DESTINATION ${DPKG_INSTALL_PREFIX})
 
-  set(CMAKE_INSTALL_PREFIX ${DPKG_INSTALL_PREFIX} 
+  set(CMAKE_INSTALL_PREFIX ${DPKG_INSTALL_PREFIX}
     CACHE PATH "Installation path for dpkgable install")
-  execute_process(COMMAND sed -e s/\@CMAKE_BINARY_DIR\@/\@CMAKE_INSTALL_PREFIX\@/g\;s/\@CMAKE_SOURCE_DIR\@/\@CMAKE_INSTALL_PREFIX\@/g 
-    INPUT_FILE ${CMAKE_SOURCE_DIR}/cmake/env-shell.sh.in 
+  execute_process(COMMAND sed -e s/\@CMAKE_BINARY_DIR\@/\@CMAKE_INSTALL_PREFIX\@/g\;s/\@CMAKE_SOURCE_DIR\@/\@CMAKE_INSTALL_PREFIX\@/g
+    INPUT_FILE ${CMAKE_SOURCE_DIR}/cmake/env-shell.sh.in
     OUTPUT_FILE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/dpkg-env-shell.sh.in)
   configure_file(
     ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/dpkg-env-shell.sh.in
@@ -290,7 +293,6 @@ add_custom_target(deploy-docs
   COMMAND rsync -va --delete ${CMAKE_BINARY_DIR}/docs/ buildmaster@dragon:/opt/docs/${DEST}/
   COMMENT Deploying docs to ${DEST}
   )
-
 
 ## coverage target
 add_custom_target(coverage
@@ -317,5 +319,3 @@ add_custom_target(pycoverage_verbose
   COMMAND ./env-shell.sh pycoverage -d pycoverage/`date +%Y-%m-%d` -v
   COMMAND rm -f pycoverage/00_LATEST \; ln -sf `ls -1tr pycoverage |tail -1` pycoverage/00_LATEST
 )
-
-
