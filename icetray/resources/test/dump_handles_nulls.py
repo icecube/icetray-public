7a56371b4566b45dfb50fbb39e5ffa9ec2999651
Alex Olivas <aolivas@umd.edu>
2016-11-16 22:11:05 +0000

    modernize test script.  previous style is like 8 years old already.
    svn path:/highlander/trunk/; revision:168775

--- icetray/resources/test/dump_handles_nulls.py
+++ icetray/resources/test/dump_handles_nulls.py
@@ -1,31 +1,21 @@
 #!/usr/bin/env python
 #
-# Checking that the driving time exists
+# Checking that the Dump handles nulls w/o crashing
 #
-import sys
 from I3Tray import *
-
 from icecube import icetray 
 
 tray = I3Tray()
 
 # generate empty frames
-tray.AddModule("BottomlessSource","bottomless")
-
-tray.AddModule("AddNulls", "nulls")(
-    ("where", ["here", "there", "everywhere", "tonsastuff"])
-    )
+tray.Add("BottomlessSource")
+tray.Add("AddNulls", where = ["here", "there", "everywhere", "tonsastuff"])
 
 # verify they are there
-tray.AddModule("FrameCheck","checker")(
-    ("ensure_physics_has", ["here", "there", "everywhere", "tonsastuff"])
-    )
+tray.Add("FrameCheck", ensure_physics_has = ["here", "there", "everywhere", "tonsastuff"])
 
 # print em.   lots of nulls, dump shouldn't crash.
-tray.AddModule("Dump","dump")
-
-# throw em out
-tray.AddModule("TrashCan","adios")
+tray.Add("Dump")
 
 tray.Execute(15)
 tray.Finish()
