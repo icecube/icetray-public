c72f9db563352c8e1bd29b085c299f6753a06b0e
nega <nega0@users.noreply.github.com>
2016-12-15 17:22:27 +0000

    more special healpix options for special fedora
    svn path:/highlander/trunk/; revision:170359

--- cmake/all_tools.cmake
+++ cmake/all_tools.cmake
@@ -4,7 +4,7 @@
 set(ALL_TOOLS pthread root python boost boostnumpy
     blas lapack gsl sprng sla libarchive
     mysql bdb mpi suitesparse ZThread omniORB ncurses cdk
-    healpix qt4 cfitsio hdf5 minuit2 clhep geant4 zlib
+    cfitsio healpix qt4 hdf5 minuit2 clhep geant4 zlib
     opencl gmp log4cpp xml2 genie zmq doxygen
     multinest nlopt
     ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
