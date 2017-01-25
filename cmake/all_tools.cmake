2af297b336e4c7ac4d65af1030239a6dbd6d8b1c
Christopher Weaver <chris.weaver@icecube.wisc.edu>
2017-01-25 00:25:09 +0000

    Add a tool definition for HepMC so that we can eventually read data from additional event generators
    svn path:/highlander/trunk/; revision:170365

--- cmake/all_tools.cmake
+++ cmake/all_tools.cmake
@@ -6,7 +6,7 @@ set(ALL_TOOLS pthread root python boost boostnumpy
     mysql bdb mpi suitesparse ZThread omniORB ncurses cdk
     cfitsio healpix qt4 hdf5 minuit2 clhep geant4 zlib
     opencl gmp log4cpp xml2 genie zmq doxygen
-    multinest nlopt
+    multinest nlopt hepmc
     ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
     )
 list(REMOVE_DUPLICATES ALL_TOOLS)
