e88260fc4c6746a9ec5f570f54ae133a84cd2a84
nega <nega0@users.noreply.github.com>
2017-06-21 21:14:22 +0000

    dont bother with `tooldef()` under system packages
    svn path:/highlander/trunk/; revision:170401

--- cmake/tools/mysql.cmake
+++ cmake/tools/mysql.cmake
@@ -17,6 +17,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>
 #  
+if(NOT SYSTEM_PACKAGES)
 TOOLDEF (mysql
     include/mysql-4.1.20
     mysql/mysql.h
@@ -24,6 +25,7 @@ TOOLDEF (mysql
     NONE
     mysqlclient
     )
+endif()
 
 if (NOT MYSQL_FOUND)
     set(MYSQL_CONFIG_ERROR)
