4ac30793cbd87494f7c5f8cd7dc8c51d5fda6704
David Schultz <davids24@gmail.com>
2017-07-10 15:29:13 +0000

    move order of includes to attempt to make mac bots happy
    svn path:/highlander/trunk/; revision:169237

--- dataio/private/shovel/Model.cxx
+++ dataio/private/shovel/Model.cxx
@@ -19,9 +19,10 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  *  
  */
+#include <regex>
+
 #include "shovel/Model.h"
 
-#include <regex>
 #include <sys/types.h>
 
 #include <boost/optional.hpp>
