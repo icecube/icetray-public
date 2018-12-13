f9003ecf9ac59d7b22601d5dd4f61be210131cf6
Alex Olivas <aolivas@umd.edu>
2018-12-13 20:17:04 +0000

    I3RecoPulseSeriesMapCombineByModule isn't default constructible.  adding it to the skip list.
    svn path:/highlander/trunk/; revision:170493

--- dataclasses/resources/test/test_disabling_of_python_dynamism.py
+++ dataclasses/resources/test/test_disabling_of_python_dynamism.py
@@ -8,7 +8,8 @@ from icecube.icetray import test_disabling_of_python_dynamism as test_freeze
 skip_these = ["I3SuperDST","I3SuperDSTTrigger","I3SuperDSTTriggerSeries","I3RecoPulseSeriesMapMask",\
               "I3RecoPulseSeriesMapUnion","DroopedSPETemplate",\
               "map_indexing_suite_map_Subdetector_I3TriggerReadoutConfig_entry",
-              "PairDoubleDouble", "I3Matrix", "I3RecoPulseSeriesMapApplySPECorrection"]
+              "PairDoubleDouble", "I3Matrix", "I3RecoPulseSeriesMapApplySPECorrection",
+              "I3RecoPulseSeriesMapCombineByModule"]
 ENSURE( test_freeze.test_all_classes(dc, skip_these) , "not cool" )
 
 print("PASS")
