a28d6efba13bf03b79b97e88bb7e960ec4c0663e
nega <nega0@users.noreply.github.com>
2019-02-14 15:49:07 +0000

    bs change to trigger buildbots
    svn path:/meta-projects/combo/trunk/; revision:171296

--- cmake/all_tools.cmake
+++ cmake/all_tools.cmake
@@ -1,12 +1,12 @@
 #
 #  These guys are in subdirectory 'tools'
 #
-set(ALL_TOOLS pthread fftw3 root python boost boostnumpy
+set(ALL_TOOLS pthread root python boost boostnumpy
     blas gsl sprng pal libarchive
     mysql mpi suitesparse ncurses cdk
     cfitsio healpix qt4 hdf5 minuit2 clhep geant4 zlib
     opencl gmp log4cpp xml2 genie zmq doxygen
-    multinest nlopt hepmc zstd
+    multinest nlopt hepmc zstd fftw3 
     ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
     )
 list(REMOVE_DUPLICATES ALL_TOOLS)
