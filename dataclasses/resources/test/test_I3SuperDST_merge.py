c5c9fd96210361195989e106228461563dc8df43
David Schultz <davids24@gmail.com>
2019-06-04 16:03:35 +0000

    test case for #2296
    svn path:/meta-projects/combo/trunk/; revision:173718

--- dataclasses/resources/test/test_I3SuperDST_merge.py
+++ dataclasses/resources/test/test_I3SuperDST_merge.py
@@ -42,6 +42,38 @@ class I3RecoPulseSeriesMapMaskTest(unittest.TestCase):
         #print(unpack)
         if any(p.width == 0 for pulses in unpack.values() for p in pulses):
             raise Exception('0 width')
+            
+
+class I3RecoPulseSeriesMapMaskTest2(unittest.TestCase):
+    def setUp(self):
+        super(I3RecoPulseSeriesMapMaskTest2, self).setUp()
+
+        # create a pulse series map with a known bad
+        # 0 width case. only two pulses.
+        self.pulses = dataclasses.I3RecoPulseSeriesMap()
+        key1 = icetray.OMKey(42, 7)
+        vec = dataclasses.I3RecoPulseSeries()
+        pulse = dataclasses.I3RecoPulse()
+        pulse.time = 10226.6
+        pulse.charge = 28.1466
+        pulse.width = 0.833585
+        vec.append(pulse)
+        pulse.time = 10227.4
+        pulse.charge = 18.5683
+        pulse.width = 0.833585
+        vec.append(pulse)
+        self.pulses[key1] = vec
+
+    def test_0_width(self):
+        #print('before')
+        #print(self.pulses)
+        sdst = dataclasses.I3SuperDST(self.pulses)
+        unpack = sdst.unpack()
+        #print('----')
+        #print('after')
+        #print(unpack)
+        if any(p.width == 0 for pulses in unpack.values() for p in pulses):
+            raise Exception('0 width')
 
 if __name__ == '__main__':
     unittest.main()
