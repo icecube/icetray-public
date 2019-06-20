fd919d0b05fc5bb69f397048a4567190a25efbd6
Alex Olivas <aolivas@umd.edu>
2019-06-20 13:04:52 +0000

    just a cleanup to the latest style
    svn path:/meta-projects/combo/trunk/; revision:174105

--- dataio/resources/test/e_read_compress_data_from_Linux-i386.py
+++ dataio/resources/test/e_read_compress_data_from_Linux-i386.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
 
-from I3Tray import *
-from os.path import expandvars
-
 import os
 import sys
 
+from I3Tray import I3Tray
+from os.path import expandvars
 from icecube import icetray, dataclasses, phys_services, dataio
 
 tray = I3Tray()
 
-tray.AddModule("I3Reader","reader", Filename = expandvars("$I3_TESTDATA/string-21/Linux-i386.i3.gz"))
-
-tray.AddModule("Dump","dump")
-
-tray.AddModule("I3Writer","writer")(
-    ("filename", "pass1_fastest.i3.gz"),
-    ("CompressionLevel", 1)
-    )
-
-
-
+tray.AddModule("I3Reader", Filename = expandvars("$I3_TESTDATA/string-21/Linux-i386.i3.gz"))
+tray.AddModule("I3Writer",
+               filename = "pass1_fastest.i3.gz",
+               CompressionLevel=1)
 tray.Execute()
 
