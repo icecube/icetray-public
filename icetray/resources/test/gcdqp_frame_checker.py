5f45483c7c8270d3f28680aae6390bd26f31b373
Alex Olivas <aolivas@umd.edu>
2019-10-09 22:17:20 +0000

    improve test for FrameChecker
    svn path:/meta-projects/combo/trunk/; revision:176002

--- icetray/resources/test/gcdqp_frame_checker.py
+++ icetray/resources/test/gcdqp_frame_checker.py
@@ -3,30 +3,48 @@
 from I3Tray import I3Tray
 from icecube import icetray 
 
+class Source(icetray.I3Module):
+    def __init__(self, context):
+        icetray.I3Module.__init__(self, context)
+
+        self.frame_types = [icetray.I3Frame.Physics,
+                            icetray.I3Frame.DAQ,
+                            icetray.I3Frame.DetectorStatus,
+                            icetray.I3Frame.Calibration,
+                            icetray.I3Frame.Geometry]        
+        
+    def Process(self):
+        frame_type = self.frame_types.pop()
+        self.PushFrame(icetray.I3Frame(frame_type))
+        
 class GCDQP(icetray.I3Module):
     def __init__(self, context):
         icetray.I3Module.__init__(self, context)
 
     def Geometry(self, frame):
         frame['I3Geometry'] = icetray.I3Bool(True)
-        
+        self.PushFrame(frame)
+                
     def Calibration(self, frame):
         frame['I3Calibration'] = icetray.I3Bool(True)
+        self.PushFrame(frame)
         
     def DetectorStatus(self, frame):
         frame['I3DetectorStatus'] = icetray.I3Bool(True)
+        self.PushFrame(frame)
         
     def DAQ(self, frame):
         frame['DAQyObject'] = icetray.I3Bool(True)
+        self.PushFrame(frame)
         
     def Physics(self, frame):
         frame['FizzyObject'] = icetray.I3Bool(True)
+        self.PushFrame(frame)
 
 tray = I3Tray()
-
-# generate empty frames
-tray.Add("BottomlessSource")
-tray.AddModule(GCDQP)
+    
+tray.Add(Source)
+tray.Add(GCDQP)
 
 tray.Add("FrameCheck",
          ensure_geometry_has = ['I3Geometry'],
@@ -37,3 +55,4 @@ tray.Add("FrameCheck",
 
 tray.Execute(5)
 
+del tray
