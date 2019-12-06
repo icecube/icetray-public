d1895b2089e6529dd2e57b2e6ebdc3f543af2844
Kevin Meagher <kmeagher@icecube.wisc.edu>
2019-12-06 16:19:44 +0000

    remove reference to qt4
    svn path:/meta-projects/combo/trunk/; revision:177731

--- cmake/all_tools.cmake
+++ cmake/all_tools.cmake
@@ -4,7 +4,7 @@
 set(ALL_TOOLS pthread root python boost boostnumpy
     blas gsl sprng pal libarchive
     mysql suitesparse ncurses cdk
-    cfitsio healpix qt4 hdf5 minuit2 clhep geant4 zlib
+    cfitsio healpix hdf5 minuit2 clhep geant4 zlib
     opencl gmp log4cpp xml2 genie zmq
     multinest nlopt zstd fftw3
     ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
