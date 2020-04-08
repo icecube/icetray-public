cc872335e635957c40e4f03b023440f11ffcff83
Jakob van Santen <jvansanten@icecube.wisc.edu>
2020-04-08 09:54:14 +0000

    Bind clsim server to random local TCP port
    This works around an issue where mktemp() uses a prefix that is too long
    to be a valid socket name on some kernels. Fixes #2422.
    
    svn path:/meta-projects/combo/trunk/; revision:179700

--- cmake/tools/zmq.cmake
+++ cmake/tools/zmq.cmake
@@ -29,6 +29,14 @@ if (SYSTEM_PACKAGES)
     )
 endif ()
 
+if (ZMQ_FOUND AND NOT ZMQ_VERSION)
+  file(READ "${ZMQ_INCLUDE_DIR}/include/zmq.h" _ZMQ_H_CONTENTS)
+  string(REGEX REPLACE ".*#define ZMQ_VERSION_MAJOR ([0-9]+).*" "\\1" ZMQ_VERSION_MAJOR "${_ZMQ_H_CONTENTS}")
+  string(REGEX REPLACE ".*#define ZMQ_VERSION_MINOR ([0-9]+).*" "\\1" ZMQ_VERSION_MINOR "${_ZMQ_H_CONTENTS}")
+  string(REGEX REPLACE ".*#define ZMQ_VERSION_PATCH ([0-9]+).*" "\\1" ZMQ_VERSION_PATCH "${_ZMQ_H_CONTENTS}")
+  set(ZMQ_VERSION "${ZMQ_VERSION_MAJOR}.${ZMQ_VERSION_MINOR}.${ZMQ_VERSION_PATCH}")
+endif ()
+
 # Detect cppzmq versions from before there were versions
 if (ZMQ_FOUND AND NOT CPPZMQ_VERSION)
   include(CheckCXXSymbolExists)
