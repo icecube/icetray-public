947c45b2119d0eb8e97f6b6ba8a423f1558ee6a7
Alex Olivas <aolivas@umd.edu>
2020-07-02 16:23:52 +0000

    go all-in with cmake find_package.  fixes #2408.
    svn path:/meta-projects/combo/trunk/; revision:180870

--- cmake/tools/ncurses.cmake
+++ cmake/tools/ncurses.cmake
@@ -21,60 +21,6 @@
 # we don't use the tooldef() macro, so we have to fudge pretty-printing
 colormsg("")
 colormsg(HICYAN "ncurses")
-
 set(CURSES_NEED_NCURSES TRUE)
-
-execute_process(COMMAND lsb_release -si OUTPUT_VARIABLE SUSE ERROR_QUIET)
-if(SUSE MATCHES "^SUSE LINUX" OR SUSE MATCHES "^openSUSE")
-  set(SUSE TRUE)
-  colormsg(YELLOW "ncurses is broken on SUSE-like distros.")
-  colormsg(YELLOW "If you need dataio-shovel, file a bug at https://code.icecube.wisc.edu/projects/icecube/newticket")
-  return()
-endif()
 find_package(Curses)
-
-# 
-if(NOT CURSES_FOUND)
-
-  set(NCURSES_FOUND FALSE)
-
-else()
-
-  set(NCURSES_FOUND TRUE)
-  set(CURSES_NCURSES_INCLUDE_PATH)
-  if(SUSE)
-    unset(CURSES_NCURSES_INCLUDE_PATH)
-    unset(CURSES_NCURSES_INCLUDE_PATH CACHE)
-  endif()
-
-  if(CURSES_HAVE_NCURSES_NCURSES_H)
-
-    get_filename_component(CURSES_NCURSES_INCLUDE_PATH ${CURSES_HAVE_NCURSES_NCURSES_H} PATH CACHE)
-    message(STATUS "+ ncurses.h found at ${CURSES_NCURSES_INCLUDE_PATH} (this should end with \"ncurses\")")
-    message(STATUS "+ libncurses found at ${CURSES_NCURSES_LIBRARY}")
-    set(NCURSES_INCLUDE_DIR ${CURSES_NCURSES_INCLUDE_PATH}
-      CACHE STRING "Include dir for tool 'ncurses'")
-
-  elseif(CURSES_HAVE_NCURSES_H)
-
-    get_filename_component(CURSES_NCURSES_INCLUDE_PATH ${CURSES_HAVE_NCURSES_H} PATH CACHE)
-    message(STATUS "+ ncurses.h found at ${CURSES_NCURSES_INCLUDE_PATH}")
-    message(STATUS "+ libncurses found at ${CURSES_NCURSES_LIBRARY}")
-    set(NCURSES_INCLUDE_DIR ${CURSES_NCURSES_INCLUDE_PATH} 
-      CACHE STRING "Include dir for tool 'ncurses'")
-
-  else(CURSES_HAVE_NCURSES_NCURSES_H)
-
-    message(STATUS "+ ncurses.h found at ${CURSES_INCLUDE_PATH}")
-    message(STATUS "+ libncurses found at ${CURSES_NCURSES_LIBRARY}")
-    set(NCURSES_INCLUDE_DIR ${CURSES_INCLUDE_PATH} 
-      CACHE STRING "Include dir for tool 'ncurses'")
-
-  endif(CURSES_HAVE_NCURSES_NCURSES_H)
-      
-  ## setting this to CURSES_LIBRARIES includes libform
-  set(NCURSES_LIBRARIES ${CURSES_LIBRARIES}
-    CACHE STRING "Libraries for tool 'ncurses'")
-
-endif(NOT CURSES_FOUND)
 
