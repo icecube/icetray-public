0d7800f830b14253f6b3409533bb5776f99cb4ab
Christian Haack <christian.haack@icecube.wisc.edu>
2020-10-15 19:57:33 +0000

    added boost numpy
    svn path:/meta-projects/combo/trunk/; revision:182650

--- cmake/tools/boost.cmake
+++ cmake/tools/boost.cmake
@@ -49,7 +49,7 @@ if(NOT DEFINED Boost_PYTHON_TYPE)
 	endif()
 endif()
 
-find_package(Boost COMPONENTS system thread date_time filesystem program_options regex iostreams ${Boost_PYTHON_TYPE} REQUIRED)
+find_package(Boost COMPONENTS system thread date_time filesystem program_options regex iostreams ${Boost_PYTHON_TYPE} numpy37 REQUIRED)
 
 if(Boost_FOUND)
 	set(BOOST_FOUND TRUE CACHE BOOL "Boost found successfully")
