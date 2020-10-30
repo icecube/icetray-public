172d1fe0ceb77a03573c04897b3f5f1a41c18684
Christopher Weaver <chris.weaver@icecube.wisc.edu>
2020-10-30 14:16:04 +0000

    Check for alternate python version suffixes.
    This does not yet cover the full range of boost library suffixes, like `-mt`, so it may need to be extended further.
    
    svn path:/meta-projects/combo/trunk/; revision:182925

--- cmake/tools/boostnumpy.cmake
+++ cmake/tools/boostnumpy.cmake
@@ -4,8 +4,30 @@ tooldef(boostnumpy
     boost/python/numpy.hpp
     lib
     NONE # The bin directory is n/a, placeholder
-    boost_numpy
+    boost_numpy${Python_VERSION_MAJOR}${Python_VERSION_MINOR}
 )
+
+# If we didn't find the library, it may be that we just guessed the 
+# wrong python version suffix, so repeat with some likely variants.
+IF (NOT BOOSTNUMPY_FOUND)
+    tooldef(boostnumpy
+        include
+        boost/python/numpy.hpp
+        lib
+        NONE # The bin directory is n/a, placeholder
+        boost_numpy${Python_VERSION_MAJOR} # Try with simple suffix
+)
+ENDIF()
+
+IF (NOT BOOSTNUMPY_FOUND)
+    tooldef(boostnumpy
+        include
+        boost/python/numpy.hpp
+        lib
+        NONE # The bin directory is n/a, placeholder
+        boost_numpy$ # Try with no suffix
+)
+ENDIF()
 
 # boost::numpy can't work without the Numpy C API
 IF (BOOSTNUMPY_FOUND)
