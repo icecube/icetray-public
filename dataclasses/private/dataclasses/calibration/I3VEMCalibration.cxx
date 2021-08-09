bf056a88049b9e2ac402d2036d2174963a44bb92
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2021-08-09 17:07:57 -0500

    fix unguarded if statement in I3VEMCalibration
    fixes #2638

--- dataclasses/private/dataclasses/calibration/I3VEMCalibration.cxx
+++ dataclasses/private/dataclasses/calibration/I3VEMCalibration.cxx
@@ -14,13 +14,13 @@ I3VEMCalibration::~I3VEMCalibration() {}
 template <class Archive>
 void I3VEMCalibration::serialize(Archive& ar, unsigned version)
 {
-  if (version>i3vemcalibration_version_)
+  if (version>i3vemcalibration_version_){
     log_fatal("Attempting to read version %u from file but running version %u of I3VEMCalibration class.",version,i3vemcalibration_version_);
-
-    ar & make_nvp("pePerVEM",      pePerVEM);
-    ar & make_nvp("muPeakWidth",   muPeakWidth);
-    ar & make_nvp("hglgCrossOver", hglgCrossOver);
-    ar & make_nvp("corrFactor",    corrFactor);
+  }
+  ar & make_nvp("pePerVEM",      pePerVEM);
+  ar & make_nvp("muPeakWidth",   muPeakWidth);
+  ar & make_nvp("hglgCrossOver", hglgCrossOver);
+  ar & make_nvp("corrFactor",    corrFactor);
 }
 
 std::ostream& operator<<(std::ostream& oss, const I3VEMCalibration& vc)
