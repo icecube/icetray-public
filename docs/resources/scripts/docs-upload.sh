1469e98ccca2074ba80d3ce57b8d094b5af87ad6
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2021-10-05 11:52:29 -0500

    fix loading sphinx extensions
    They need to be a list not a generator
    
    Also, use an env var for the password to upload docs to the server so you don't have to edit the file

--- docs/resources/scripts/docs-upload.sh
+++ docs/resources/scripts/docs-upload.sh
@@ -3,8 +3,11 @@
 # This Script will upload the documnentation created by docs-build to the 
 # documentation server (http://docs.icecube.aq).
 # First it creates a tarball then it uses curl to access the REST interface on the server
-# For safty reasons, this script is disabled by default, to actually upload you must replace
-# "XXXXXXXX" with the standard password
+
+if [ -z "${ICECUBE_PASSWORD}" ]; then
+    echo ICECUBE_PASSWORD not set, aborting
+    exit -1
+fi
 
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 GIT_REVISION=`git rev-parse --verify --short HEAD`
@@ -14,6 +17,6 @@ TARCMD="tar -czvf $TARBALL_NAME -C${I3_BUILD}/docs/build/html ."
 echo running $TARCMD
 ${TARCMD}
 
-UPLOADCMD="curl -XPUT -i --data-binary @${TARBALL_NAME} https://docs.icecube.aq/api/upload?path=icetray/main -u icecube:XXXXXXXX"
+UPLOADCMD="curl -XPUT -i --data-binary @${TARBALL_NAME} https://docs.icecube.aq/api/upload?path=icetray/main -u icecube:${ICECUBE_PASSWORD}"
 echo running ${UPLOADCMD}
 ${UPLOADCMD}
