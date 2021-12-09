c09032fb1c91f7ac3eee155239a5f861f83980d9
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2021-12-09 10:58:37 -0600

    fix test failure caused by rounding in I3Random.integer()
    The use of inclusive limits in I3Random.integer() causes values to be
    rounded down on some compilers. Test the output against the value in
    the spec and the number one less

--- phys-services/resources/test/test_MT19937_value.py
+++ phys-services/resources/test/test_MT19937_value.py
@@ -3,6 +3,9 @@ from icecube.phys_services import I3MT19937
 
 # the docs for the generator specifies the the 10,000th value
 # of the generator is 4123659995 for the default constructor
+# However, because I3Random.integer has an inclusive upper limit you cannot
+# test the exact value and on some compilers it might get rounded down to the 
+# next integer
 default_mt19937 = I3MT19937()
 r = [default_mt19937.integer(2**32-1) for x in range(10000)]
-assert(r[-1]==4123659995)
+assert(r[-1] in [4123659994, 4123659995])
