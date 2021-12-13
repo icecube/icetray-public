68e7b6553572792da8c007bf2f9e87728565372d
Alex Olivas <aolivas@umd.edu>
2021-12-13 15:41:03 -0700

    update the contributor count script for use with github

--- docs/release_contribution_count.py
+++ docs/release_contribution_count.py
@@ -1,30 +1,53 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
-
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('start_revision',
-                    type=int,
-                    help='Starting revision number')
-parser.add_argument('stop_revision',
-                    type=int,
-                    help='Stopping revision number')
-args = parser.parse_args()
-
+import logging
 import os
 import subprocess
 
-command = ['svn', 'log',
-           os.environ['I3_SRC'],
-           '-r%d:%d' % (args.start_revision, args.stop_revision)]
+parser = argparse.ArgumentParser(description='Count contributors between git revisions.')
+parser.add_argument('start_revision', help='Starting revision number')                    
+parser.add_argument('stop_revision', help='Stopping revision number')                    
+args = parser.parse_args()
 
+logging.basicConfig(level=logging.INFO)
+
+def _extract_author(git_log_line):
+    # Given a line like:
+    #     Author: kathrawlins <78505717+kathrawlins@users.noreply.github.com>
+    #         ...or...
+    #     Author: Jakob van Santen <jvansanten@gmail.com>
+    # return the contributor name
+    # Essentially this function strips the leading "Author: " and trailing email.
+    if 'Author: ' in git_log_line:
+        result = git_log_line[len('Author: '):]
+        idx = result.find('<')
+        return result[:idx].strip()
+
+def parse_contributors(git_log):
+    # from the git log returns a set of contributor
+    result = set()
+    for line_byte in output.splitlines():
+        line = line_byte.decode('utf-8')        
+        contributor = _extract_author(line)
+        if contributor:
+            result.add(contributor)
+    return result
+    
+if 'I3_SRC' not in os.environ:
+    logging.fatal('I3_SRC not set.')
+I3_SRC = os.environ['I3_SRC']
+
+command = \
+    ['git', 'log',
+    '--mailmap',
+     '%s..%s' % (args.start_revision, args.stop_revision),
+     I3_SRC
+    ]
 output = subprocess.check_output(command)
 
-contributors = set()
-for line_byte in output.splitlines():
-    line = line_byte.decode('utf-8')
-    if line.count('|') == 3:
-        contributor = line.split('|')[1]
-        contributors.add(contributor.strip())
-print(','.join(contributors))
-print('Contributor count = %d' % len(contributors))
+contributors = parse_contributors(output)
+
+for contributor in contributors:
+    logging.info('    %s' % contributor)
+logging.info('Contributor count = %d' % len(contributors))
