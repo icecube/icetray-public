2c6ca4db0f36a1b6e0e7c498b5f449af35e2a197
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2021-12-13 21:45:33 -0600

    remove reference to long ago deleted genie tool

--- cmake/all_tools.cmake
+++ cmake/all_tools.cmake
@@ -5,7 +5,7 @@ set(ALL_TOOLS pthread root python boost boostnumpy
     blas gsl sprng pal libarchive
     suitesparse ncurses cdk
     cfitsio healpix hdf5 minuit2 clhep geant4 zlib
-    opencl gmp genie zmq
+    opencl gmp zmq
     multinest nlopt zstd fftw3
     ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
     )
