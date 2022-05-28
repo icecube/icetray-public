a2fda4c764d273203a28e857b5bda6da6d7c6c65
Torsten Schmidt <schlemihl@users.noreply.github.com>
2022-05-28 23:56:57 +0200

    - adding test for I3XMLOMKey2MBID service, new DM Ice mainboards, merge functionality

--- phys-services/private/test/I3XMLOMKey2MBIDTest.cxx
+++ phys-services/private/test/I3XMLOMKey2MBIDTest.cxx
@@ -12,6 +12,9 @@
 
 #include <exception>
 #include <string>
+#include <map>
+
+#include <boost/filesystem.hpp>
 
 #include <interfaces/I3OMKey2MBID.h>
 #include <phys-services/I3XMLOMKey2MBID.h>
@@ -52,10 +55,10 @@ TEST(b_operations)
   I3OMKey2MBIDPtr omkey2mbid;
   OMKey key;
   long long int mbid;
-  
+
   initI3XMLOMKey2MBIDTest(omkey2mbid, "test_basic_operations_mainboard_ids.xml");
-  
-  key = omkey2mbid->GetOMKey(3); 
+
+  key = omkey2mbid->GetOMKey(3);
   ENSURE_EQUAL(key.GetString(), 1);
   ENSURE_EQUAL(static_cast<int>(key.GetOM()), 3);
 
@@ -67,4 +70,46 @@ TEST(b_operations)
 
   key = OMKey(1, 33);
   EXPECT_THROW(omkey2mbid->GetMBID(key), "didn't throw!");
+}
+
+
+TEST(c_DMIce)
+{
+  I3OMKey2MBIDPtr omkey2mbid;
+  OMKey key;
+  long long int mbid;
+
+  initI3XMLOMKey2MBIDTest(omkey2mbid, "mainboard_ids.xml.gz");
+
+  ENSURE_EQUAL(omkey2mbid->GetMBID(OMKey(0, 2)), 0x8c40d705fc4b);
+  ENSURE_EQUAL(omkey2mbid->GetMBID(OMKey(0, 3)), 0x37793e5448e1);
+  ENSURE_EQUAL(omkey2mbid->GetMBID(OMKey(0, 4)), 0x99b591c21d6e);
+  ENSURE_EQUAL(omkey2mbid->GetMBID(OMKey(0, 5)), 0x04f1d6939870);
+}
+
+
+TEST(d_Merge)
+{
+  I3OMKey2MBIDPtr omkey2mbid;
+  initI3XMLOMKey2MBIDTest(omkey2mbid, "test_basic_operations_mainboard_ids.xml");
+  EXPECT_THROW(omkey2mbid->GetMBID(OMKey(0, 2)), "didn't throw");
+
+  map<long long int, OMKey> conversionTable;
+  conversionTable[0x8c40d705fc4b] = OMKey(0, 2);
+  conversionTable[0x37793e5448e1] = OMKey(0, 3);
+
+  ENSURE(getenv("I3_SRC"));
+  string infile(getenv("I3_SRC"));
+  ENSURE(!infile.empty());
+  infile.append("/phys-services/resources/test_basic_operations_mainboard_ids.xml");
+  boost::filesystem::path temp = (boost::filesystem::temp_directory_path() /
+                                  boost::filesystem::unique_path());
+  string outfile = temp.native();
+  I3XMLOMKey2MBID::Merge(outfile, infile, conversionTable);
+
+  omkey2mbid = I3OMKey2MBIDPtr(new I3XMLOMKey2MBID(outfile));
+  ENSURE_EQUAL(omkey2mbid->GetMBID(OMKey(0, 2)), 0x8c40d705fc4b);
+  ENSURE_EQUAL(omkey2mbid->GetMBID(OMKey(0, 3)), 0x37793e5448e1);
+
+  boost::filesystem::remove(outfile);
 }
