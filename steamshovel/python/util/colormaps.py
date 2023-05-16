4c5f0dfc51d3084dcc93e8f106327da8a1317d84
nega <nega0@users.noreply.github.com>
2023-05-16 14:50:03 -0400

    steamshovel - update colormaps to new matplotlib api
    Prevent crashes with newish matplotlibs when trying to edit colormaps.
    Also makes `colormaps.py` its own test.
    
    Closes #3071

--- steamshovel/python/util/colormaps.py
+++ steamshovel/python/util/colormaps.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # The list of matplotlib color maps considered to be not useful in steamshovel
 _bad_maps = ['Accent', 'Dark2', 'Paired', 'Pastel1', 'Pastel2',
              'Set1', 'Set2', 'Set3', 'gist_earth', 'gist_ncar',
@@ -20,10 +22,10 @@ def make_custom_maps():
     # adapted from
     # http://matplotlib.org/examples/pylab_examples/custom_cmap.html
     try:
-        from matplotlib import colors, cm
+        from matplotlib import colors, cm  # noqa: F401
         from matplotlib.colors import LinearSegmentedColormap
-    except ImportError as e:
-        print("Could not make custom maps")
+    except ImportError:
+        print("Could not make custom maps: ")
         return
 
     cdict1 = {'red':   ((0.0, 0.0, 0.0),
@@ -53,15 +55,21 @@ def make_custom_maps():
                         (1.0, 0.0, 0.0))
               }
 
-    cm.register_cmap(name='BlueRed1', data=cdict1)
-    cm.register_cmap(name='Belgium', data=cdict2)
-
+    try:
+        # matplotlib 3.7+
+        from matplotlib import colormaps
+        colormaps.register(LinearSegmentedColormap('BlueRed1', cdict1))
+        colormaps.register(LinearSegmentedColormap('Belgium',  cdict2))
+    except ImportError:
+        # at least matplotlib 2.2.4, maybe earlier
+        cm.register_cmap(cmap=LinearSegmentedColormap('BlueRed1', cdict1))
+        cm.register_cmap(cmap=LinearSegmentedColormap('Belgium',  cdict2))
 
 def get_matplotlib_colormaps():
     try:
         from matplotlib import cm
-    except ImportError as e:
-        print("Could not import matplotlib, so won't grab any color maps")
+    except ImportError:
+        print("Could not import matplotlib, so won't grab any color maps: ")
         return []
 
     make_custom_maps()
@@ -71,12 +79,24 @@ def get_matplotlib_colormaps():
         ignore.add(r[:-2] if r.endswith("_r") else r + "_r")
 
     ret = []
-    for name in sorted(cm.cmap_d):
+
+    try:
+        # matplotlib 3.7+
+        from matplotlib import colormaps
+        cmaps = sorted(colormaps)
+        def get_cmap_name(name):
+            return colormaps[name]
+    except ImportError:
+        cmaps = sorted(cm.cmap_d)
+        def get_cmap_name(name):
+            return cm.get_cmap(name)
+
+    for name in cmaps:
         # avoid ugly maps and their reversals
         name_base = name[:-2] if name.endswith("_r") else name
         if name_base in _bad_maps or name in ignore:
             continue
-        m = cm.get_cmap(name)
+        m = get_cmap_name(name)
         # warning: this code uses a private data member,
         # this unsafe and may lead to conflicts in the future
         if not hasattr(m, '_segmentdata'):
@@ -96,3 +116,17 @@ def get_matplotlib_colormaps():
         ret.append((name, rgb))
 
     return ret
+
+
+if __name__ == "__main__":
+    #from pprint import pprint
+    import matplotlib
+
+    try:
+        c = get_matplotlib_colormaps()
+        #pprint(c)
+    except Exception as e:
+        import sys
+        print(f"{matplotlib.__version__=}")
+        print("Something failed: ", e)
+        sys.exit(1)
