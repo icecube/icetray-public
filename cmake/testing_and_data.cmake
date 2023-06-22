754317a47b4265b60f4e8ba60a0e0da2f2061c3e
nega <nega0@users.noreply.github.com>
2023-06-22 13:20:31 -0400

    point test-data to a release specific version

--- cmake/testing_and_data.cmake
+++ cmake/testing_and_data.cmake
@@ -19,7 +19,7 @@ colormsg("")
 colormsg(_HIBLUE_ "Configuring data and tests...")
 colormsg("")
 
-set(TESTDATA_VERSION "trunk")
+set(TESTDATA_VERSION "releases/V01-07-00")
 set(PRODDATA_VERSION "trunk")
 
 ## set the value of I3_TESTDATA
