c7e9b549d59a56191add9a4b802e9a1c5e0455a6
Jakob van Santen <jvansanten@gmail.com>
2023-10-12 20:14:11 +0200

    Attach tableio converter submodule to current module (#3195)
    * Attach tableio converter submodule to current module
    
    Fixes #3187
    
    * Allow tableio namespace outside module definition
    
    e.g. in tests, where scope is None and None has no name

--- tableio/public/tableio/converter/pybindings.h
+++ tableio/public/tableio/converter/pybindings.h
@@ -28,7 +28,7 @@ namespace bp = boost::python;
 // in tableio into the dataclasses module
 
 #define I3CONVERTER_NAMESPACE(proj)                                                                                \
-    bp::object module(bp::handle<>(bp::borrowed(PyImport_AddModule("icecube._" BOOST_PP_STRINGIZE(proj)))));\
+    bp::object module((bp::scope() && bp::scope().attr("__name__") == "icecube._" BOOST_PP_STRINGIZE(proj)) ? bp::scope() : bp::object(bp::handle<>(bp::borrowed(PyImport_AddModule("icecube._" BOOST_PP_STRINGIZE(proj))))));\
     bp::object converter_module(bp::handle<>(bp::borrowed(PyImport_AddModule("icecube._" BOOST_PP_STRINGIZE(proj) ".converters"))));\
     module.attr("converters") = converter_module;\
     bp::object tableio(bp::handle<>(PyImport_Import(bp::str("icecube.tableio").ptr())));\
