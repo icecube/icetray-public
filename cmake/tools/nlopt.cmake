8e0b050a92fb9b8e228f00f5b088377851fe048d
nega <nega0@users.noreply.github.com>
2024-04-15 14:35:26 -0400

    update nlopt detection (#3457)
    * cmake - update nlopt detection
    
    * add QUIET flag to find_package

--- cmake/tools/nlopt.cmake
+++ cmake/tools/nlopt.cmake
@@ -1,9 +1,20 @@
-#
-#  $Id: sla.cmake 2354 2015-11-30 16:36:25Z kjmeagher $
-#
 #  Copyright (C) 2016 the IceCube Collaboration <http://www.icecube.wisc.edu>
 #  SPDX-License-Identifier: BSD-2-Clause
 
+# nlopt has first class CMake support, but debian/ubuntu split the C++ bits
+# into its own package. while ``find_package(NLopt)`` finds everything, the
+# resultant target is ``NLopt::nlopt_cxx`` on these systems.
+
+find_package(NLopt QUIET)
+if(NLopt_FOUND)
+  set(NLOPT_FOUND TRUE CACHE BOOL "Tool 'nlopt' found successfully" FORCE)
+  # we don't use the tooldef() macro, so we have to fudge pretty-printing
+  colormsg("")
+  colormsg(HICYAN "nlopt")
+  message(STATUS "+  version: ${NLOPT_VERSION}")
+  message(STATUS "+ includes: ${NLOPT_INCLUDE_DIRS}")
+  message(STATUS "+     libs: ${NLOPT_LIBRARY_DIRS}")
+else()
 TOOLDEF (nlopt
   include
   nlopt.hpp
@@ -11,3 +22,4 @@ TOOLDEF (nlopt
   NONE
   nlopt
   )
+endif()
