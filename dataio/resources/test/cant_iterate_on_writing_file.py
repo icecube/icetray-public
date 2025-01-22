2ea0c5d0659efe912d4eaac30a6c26a623745d0d
nega <nega0@users.noreply.github.com>
2025-01-22 16:34:18 -0500

    dataio - expand test and actually delete temp data

--- dataio/resources/test/cant_iterate_on_writing_file.py
+++ dataio/resources/test/cant_iterate_on_writing_file.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-
 # SPDX-FileCopyrightText: 2024 The IceTray Contributors
 #
 # SPDX-License-Identifier: BSD-2-Clause
 
 #
-#  If an I3File is opened Writing it won't give you an iterator.
+# Ensure that if an I3File is opened for writing it won't give you an iterator.
 #
-from icecube import icetray, dataclasses, dataio
+from icecube import dataio
 import sys, os
 
 fn = "alphabet2.i3"
@@ -16,12 +15,15 @@ i3f = dataio.I3File(fn, 'w')
 try:
     for frame in i3f:
         print(frame)
-except:
+except RuntimeError:
     print("ok, that threw as expected")
     sys.exit(0)
+except Exception as e:
+    print("FAIL: that threw with an unexpected Exception")
+    raise(e)
 else:
     print("FAIL: that should have thrown")
     sys.exit(1)
-
-if os.path.exists(fn):
-    os.unlink(fn)
+finally:
+    if os.path.exists(fn):
+        os.unlink(fn)
